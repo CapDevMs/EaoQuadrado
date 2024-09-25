5463de7b6106afbbf4d48f9e63a2626bd9095738
PauloMatheusSenac <paulo54098136@aluno.ms.senac.br>
2024-09-25 08:36:55 -0400

    criação DB + tabela produto 25-09-2024 08:00
    Aqui fica o script inicial de criação do DB + criação da tabela de produto

---
+++ database/BD EAoQuadrado 25-09-24 v1.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE EAoQuadrado;
+USE EAoQuadrado;
+
+CREATE TABLE Lojas (
+idloja INT PRIMARY KEY
+);
+
+CREATE TABLE Categorias (
+idcategoria INT PRIMARY KEY
+);
+
+
+CREATE TABLE Produtos (
+    idproduto INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    marca VARCHAR(100),
+    preco DECIMAL(10, 2) NOT NULL,
+    imagens JSON,  
+    cores VARCHAR(255),  
+    quantidade INT NOT NULL,
+    loja INT,  
+    categoria INT,  
+    frete DECIMAL(10, 2),  
+    FOREIGN KEY (loja) REFERENCES Lojas(idloja),  
+    FOREIGN KEY (categoria) REFERENCES Categorias(idcategoria)  
+);
+
+
+
+
+
