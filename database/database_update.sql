d93ee0f47cf01f16cfa3839dac7aadca5ab80e8f
H3nriqueSB <henrique49880516@aluno.ms.senac.br>
2024-09-27 11:21:25 -0300

    aa
    aa

--- database/database_update.sql
+++ database/database_update.sql
@@ -1,9 +1,30 @@
 create database e_ao_quadrado;
 use e_ao_quadrado;
 
-create table usuarios(
-    id primary key unsigned auto_increment,
-    nome varchar(255) not null,
+create table administrador(
+    id_adm int primary key auto_increment,
+    id_usuario varchar(255) not null,
     email varchar(255) not null unique,
-    senha varchar(255) not null
+    senha varchar(255) not null,
+    nome_adm varchar(100) not null,
+    sobrenome_adm varchar(100) not null,
+    data_nasc date not null,
+    cpf int not null unique, 
+    FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo)
+);
+
+CREATE TABLE cargo (
+    id_cargo INT PRIMARY KEY AUTO_INCREMENT,
+    cargo VARCHAR(100) not null
+);
+
+CREATE TABLE permissao (
+    id_permissao INT PRIMARY KEY AUTO_INCREMENT,
+    permissao varchar (100) not null,
+    descricao varchar (255) not null
+);
+
+CREATE TABLE relpermissao (
+FOREIGN KEY (id_adm) REFERENCES administrador(id_adm),
+FOREIGN KEY (id_permissao) REFERENCES permissao(id_permissao)
 );
