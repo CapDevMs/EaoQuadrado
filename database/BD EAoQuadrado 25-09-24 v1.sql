30b3c3aa19e20befa9fd570c75a1224ffa5553fa
Felipe Braiani <felipe.braianitech@gmail.com>
2024-09-30 08:37:48 -0400

    Atualizado script Produtos

--- database/BD EAoQuadrado 25-09-24 v1.sql
+++ database/BD EAoQuadrado 25-09-24 v1.sql
@@ -1,34 +1,29 @@
 CREATE DATABASE EAoQuadrado;
+
 USE EAoQuadrado;
 
 CREATE TABLE Lojas (
-idloja INT PRIMARY KEY
+    id_loja INT PRIMARY KEY auto_increment
 );
 
 CREATE TABLE Categorias (
-idcategoria INT PRIMARY KEY,
-nome VARCHAR (255),
-descricao VARCHAR (100)
+    id_categoria INT PRIMARY KEY auto_increment,
+    nome VARCHAR(255),
+    descricao VARCHAR(100)
 );
-
 
 CREATE TABLE Produtos (
-    idproduto INT AUTO_INCREMENT PRIMARY KEY,
+    id_produto INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    descricao VARCHAR (255),
+    descricao VARCHAR(255),
     marca VARCHAR(100),
     preco DECIMAL(10, 2) NOT NULL,
-    imagens JSON,  
-    cores VARCHAR(255),  
+    imagens JSON,
+    cores VARCHAR(255),
     quantidade INT NOT NULL,
-    loja INT,  
-    categoria INT,  
-    frete DECIMAL(10, 2),  
-    FOREIGN KEY (loja) REFERENCES Lojas(idloja),  
-    FOREIGN KEY (categoria) REFERENCES Categorias(idcategoria)  
+    id_loja INT,
+    id_categoria INT,
+    frete DECIMAL(10, 2),
+    FOREIGN KEY fk_loja(id_loja) REFERENCES Lojas(id_loja),
+    FOREIGN KEY fk_categoria(id_categoria) REFERENCES Categorias(id_categoria)
 );
-
-
-
-
-
