8c9edcef2115d2bd14ccb7ddf7a8320ec3a29217
Anna Lorena <annalorena1998@gmail.com>
2024-10-02 08:33:07 -0400

    script edit_site_adm finalizado

--- database/edit_site_adm.sql
+++ database/edit_site_adm.sql
@@ -1,21 +1,24 @@
 create database e_ao_quadrado;
 use e_ao_quadrado;
 
-create table usuarios(
-    id primary key unsigned auto_increment,
+create table usuario(
+    id_usuario primary key unsigned auto_increment,
     nome varchar(255) not null,
     email varchar(255) not null unique,
     senha varchar(255) not null
 );
 
-CREATE TABLE Administrador (
+CREATE TABLE admin (
 	id_administrador INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT,
-    FOREIGN KEY fk_usuario(id_usuario) REFERENCES Usuarios(id_usuario) 
+    FOREIGN KEY fk_usuario(id_usuario) REFERENCES usuarios(id_usuario) 
 );
 
 CREATE TABLE edit_site(
-    id_administrador int,
+    id_admin int not null,
+    imagem varchar(255),
+    descricao varchar(255)
+    
     FOREIGN KEY fk_administrador(id_administrador) REFERENCES Administrador(id_administrador) 
 
 );
