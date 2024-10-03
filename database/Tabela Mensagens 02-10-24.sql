a6008fd575a39afb20146366802b0ae71fed9376
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-03 08:15:17 -0400

    Ajuste de pastas e arquivos

--- database/Tabela Mensagens 02-10-24.sql
+++ database/Tabela Mensagens 02-10-24.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Mensagens (
     id_mensagem INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR (100) NOT NULL,    
-    email VARCHAR (100),
+    email VARCHAR (100) NOT NULL,
     telefone VARCHAR(50),
-    mensagem TEXT,
+    mensagem TEXT NOT NULL,
     data_envio DATETIME
 );
