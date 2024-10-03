5a7855ce7467deaac8a1a897be4c43c4765ebcd7
Felipe Braiani <felipe.braianitech@gmail.com>
2024-10-03 08:21:34 -0400

    Merge tabela Mensagens no código principal

--- database/database_update.sql
+++ database/database_update.sql
@@ -199,6 +199,15 @@ CREATE TABLE Trocas (
 );
 
 
+CREATE TABLE Mensagens (
+    id_mensagem INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR (100) NOT NULL,    
+    email VARCHAR (100) NOT NULL,
+    telefone VARCHAR(50),
+    mensagem TEXT NOT NULL,
+    data_envio DATETIME
+);
+
 -- DELIMITER //
 -- CREATE TRIGGER valida_vendedor
 -- AFTER INSERT ON vendedores
