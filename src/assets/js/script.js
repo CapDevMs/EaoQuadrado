b9193f79863dd473d9928367e203f3683d179e99
RiquelmeG22 <riquelmeramalho762@gmail.com>
2025-01-22 11:00:27 -0400

    terminando o js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,8 @@
-function toggleDetails(cardItems) {
-    card.classList.toggle("expanded");
-  }
-  
+/**
+ * 
+ * @param {HTMLElement} element 
+ */
+function toggleDetails(element) {
+  console.log(element)
+  element.children[6].classList.toggle("expanded");
+}
