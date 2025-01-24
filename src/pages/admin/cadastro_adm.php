e8e1fbca12177982db50a091dd9e15dae735a81e
Vatreax <rafasmontiel13@gmail.com>
2025-01-24 07:38:51 -0400

    ajustes

--- src/pages/admin/cadastro_adm.php
+++ src/pages/admin/cadastro_adm.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,6 +66,8 @@
 
                 </div>
 
+                
+
                 <div class="inform1-container">
                     <h2>CPF</h2>
                     <input type="text" name="cpf" class="input2-container" placeholder="000.000.000-00" required>
@@ -112,83 +115,10 @@
                 </div>
 
  
-
-                <div class="buttons-container">
-                    <button class="button-confirm">Registrar</button>
-                </div>
+                <button class="button-confirm">Registrar</button>
             </div>
-    
+    <?php get_footer() ?>
     </main>
-
-    <footer>
-        <div class="footer-links">
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Suporte</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Senac Hub Academy</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">eaoquadado@gmail.com</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">+55 67 99999-9999</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">
-                                R. do Parque, 75 - Centro Campo Grande - MS
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Conta</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Minha Conta</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Entre/Cadastre</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Carrinho</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Lista de Desejos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Acesso</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Nossa Política</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Termos de Uso</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">FAQ</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Contato</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
 </body>
 <script src="../../assets/js/script.js"></script>
 </html>
