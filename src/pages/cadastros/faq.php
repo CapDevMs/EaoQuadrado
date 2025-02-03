248d348cfc7f3530cdadb8e25201f4929c38eb68
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2025-02-03 08:35:22 -0400

    atualizado para php

--- src/pages/cadastros/faq.php
+++ src/pages/cadastros/faq.php
@@ -1,13 +1,17 @@
+<?php
+    include('../../config/funcoes.php');
+    $appName = get_app_name();
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../assets/css/base.css">
-    <link rel="stylesheet" href="../../assets/css/cadastros/faq.css">
-    <title>Eao Quadrado</title>
+    <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
+    <?php get_css(['cadastros/faq']) ?>
 </head>
+<body>
+    <?php get_header() ?>
 <body>
     <header class="header">
         <nav class="navbar">
