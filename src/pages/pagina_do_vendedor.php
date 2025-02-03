80bdb4b8a62ae566e365c40502986cbd57f4a542
Felipe Braiani <felipe.braianitech@gmail.com>
2025-02-03 11:09:09 -0400

    Alguns ajustes na tela

--- src/pages/pagina_do_vendedor.php
+++ src/pages/pagina_do_vendedor.php
@@ -1,40 +1,44 @@
 <?php
-    include(__DIR__ . '/../config/funcoes.php');
-    $appName = get_app_name();
+include(__DIR__ . '/../config/funcoes.php');
+$appName = get_app_name();
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
     <?php get_css(['cliente/pagina_vendedor']) ?>
 </head>
+
 <body>
     <?php get_header(); ?>
 
     <main class="row">
-        <asside class="sidebar col-3 col-sm-hidden">
+        <!-- <asside class="sidebar col-3 col-sm-hidden">
             <div class="container">
                 <span>Local para receber o sidebar</span>
             </div>
-        </asside> 
+        </asside>  -->
 
-        <section class="col-9 col-sm-12">
+        <section class="col-12">
             <div class="container">
                 <div class="row">
                     <div class="col-12 pb-3">
-                        <img src="../assets/img/Banner_loja.png" class="img" alt="Banner da Loja"> 
+                        <img src="../assets/img/Banner_loja.png" class="img" alt="Banner da Loja">
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 pb-4">
                         <div class="card">
-                            <div class="card-body">
-                                <div class="col-2">
-                                    <p>imagem aqui</p>
+                            <div class="card-body justify-content-center">
+                                <div class="col-2 justify-content-center">
+                                    <div class="avatar">
+                                        <img src="../assets/img/cliente/studio_center.svg" alt="" class="figure-img img-fluid">
+                                    </div>
                                 </div>
-                                <div class="col-8">
+                                <div class="col-7">
                                     <div class="row">
                                         <div class="col-12">
                                             <h2>Studio Center</h2>
@@ -52,15 +56,15 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-3">
                                     <div class="row">
                                         <div class="col-12">
                                             <p>Avaliações: </p>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col-2">
-                                            <img src="" alt="">
+                                    <div class="row justify-content-center">
+                                        <div class="col-2 whatsapp">
+                                            <img src="../assets/img/icons/whatsapp.svg" alt="">
                                         </div>
                                         <div class="col-10">
                                             <a target="_blank" href="https://wa.me/556734377000">(67) 3437-7000</a>
@@ -81,6 +85,7 @@
     </main>
 
 
-    <?php get_footer(); ?>    
+    <?php get_footer(); ?>
 </body>
+
 </html>
