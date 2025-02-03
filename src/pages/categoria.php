bf23f49e5cf9b29305870d42d4302322955e41eb
Ricardo <sousasilvaricardo9@gmail.com>
2025-02-03 09:31:01 -0400

    trocando página para php

--- src/pages/categoria.php
+++ src/pages/categoria.php
@@ -1,121 +1,80 @@
 <?php
-require_once('./config/funcoes.php');
-require('./config/conexao.php');
-
+    include('../config/funcoes.php');
+    $appName = get_app_name();
 ?>
-<?php get_base_head(); ?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
+    <?php get_css(['categoria']) ?>
+</head>
 <body>
-    <header class="header">
-        <nav class="navbar">
-            <div class="navbar-logo">
-                <a href="#">
-                    <img src="./assets/img/logo.png" class="logo" alt="Logo">
-                </a>
-            </div>
-            <div class="search-bar">
-                <input type="text" class="search-input" placeholder="O que você procura hoje?">
-                <img src="./assets/img/pesquisar.svg" class="search-logo" alt="Pesquisar">
-            </div>
-            <ul class="menu-opcoes">
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/pessoa.svg" alt="Pessoa">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/carrinho.svg" alt="Carrinho">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/informacao.svg" alt="Informações">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/suporte.svg" alt="suporte">
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <?php get_header() ?>
 
     <main>
-        <div class="container">
-            <h1>Conteúdo da página</h1>
+        <div class="container-video">
+            <video autoplay muted loop>
+                <source src="../assets/videos/video-categoria_celular.mp4.mp4" type="video/mp4">        
+            </video>      
+        </div> 
+        <div class="categoria">
+            <button>
+                <img src="../assets/img/CATEGORIA/Categoria-celular.png"id="Categoria-celular" alt="categoria-celular">
+            </button> 
+            <button>
+                <img src="../assets/img/CATEGORIA/Categoria-computador.png" id="Categoria-computador" alt="Categoria-computador">
+            </button>
+            <button>   
+                <img src="../assets/img/CATEGORIA/Categoria-smartwatche.png" id="Categoria-smartwatche" alt="Categoria-smartwache">
+            </button>
+            <button>
+                <img src="../assets/img/CATEGORIA/Categoria-camera.png" alt="Categoria-camera" id="Categoria-camera">
+            </button>
+            <button>    
+                <img src="../assets/img/CATEGORIA/Categoria-fone_de_ouvido.png" alt="Categoria fone de ouvido" id="Categoria-fone_de_ouvido">
+            </button>
+            <button>    
+                <img src="../assets/img/CATEGORIA/Categoria-gamer.png"alt="categoria gamer" id="Categoria-gamer">
+            </button>    
         </div>
+
+        <div class="marcas_preco">
+            <h1 class="marcas">Marcas</h1>
+            <input class="search-bar-marcas" placeholder="Buscar Produtos">
+            <button class="search-marca" alt="Pesquisar_Marca"></button>
+            <h1 class="preco">Preço</h1>
+            <label for="minimo" class="label-min_max" id="min">Mínimo</label>
+            <label for="maximo" class="label-min_max" id="max">Máximo</label>
+            <input type="text" id="minimo" class="min_max">
+            <input type="text" id="maximo" class="min_max">
+        </div>
+        
+        <div class="categorias">
+            <h3>Categorias</h3>
+            <a href="">Celulares</a>
+            <a href="">Computadores</a>
+            <a href="">Smartwatches</a>
+            <a href="">Câmera</a>
+            <a href="">Fones de Ouvido</a>
+            <a href="">Games</a>
+        </div>
+        <h1 class="titulo">Avaliação dos Vendedores</h1>
+        <div class="checkbox-container"> 
+            
+            <input type="checkbox" name="tudo"><label for="tudo">Tudo</label>
+            <input type="checkbox" name="5-star"><label for="5-star" id="star5"><img src="../assets/img/CATEGORIA/Five_Star.png" id="5_star" alt=""></label>
+            <input type="checkbox" name="5-star"><label for="5-star" id="star4"><img src="../assets/img/CATEGORIA/Four_Star.png" id="5_star" alt=""></label>
+            <input type="checkbox" name="5-star"><label for="5-star" id="star3"><img src="../assets/img/CATEGORIA/Three_Star.png" id="5_star" alt=""></label>
+            <input type="checkbox" name="5-star"><label for="5-star" id="star2"><img src="../assets/img/CATEGORIA/Two_Star.png" id="5_star" alt=""></label>
+            <input type="checkbox" name="5-star"><label for="5-star" id="star1"><img src="../assets/img/CATEGORIA/One_Star.png" id="5_star" alt=""></label>
+        </div>
+        
+       
     </main>
 
-    <footer>
-        <div class="footer-links">
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Suporte</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Senac Hub Academy</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">eaoquadado@gmail.com</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">+55 67 99999-9999</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">
-                                R. do Parque, 75 - Centro Campo Grande - MS
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Conta</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Minha Conta</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Entre/Cadastre</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Carrinho</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Lista de Desejos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Acesso</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Nossa Política</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Termos de Uso</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">FAQ</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Contato</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php get_footer() ?>
 </body>
-<script src="assets/js/script.js"></script>
+<script src="../assets/js/script.js"></script>
 </html>
