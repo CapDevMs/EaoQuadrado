668676df2603d47a7cf8f27d9f243acc5caf4a57
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2025-02-05 10:49:10 -0400

    criação da pagina esqueci senha + css

---
+++ src/pages/cadastros/esqueci-senha.php
@@ -0,0 +1,33 @@
+<?php
+    include('../../config/funcoes.php');
+    $appName = get_app_name();
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
+    <?php get_css(['cadastros/esqueci-senha']) ?>
+</head>
+<body>
+    <?php get_header() ?>
+<main>
+    <div class="menu" id="home">Home</div>
+    <span class="menu">/</span>
+    <div class="menu">Esqueci a Senha</div>
+    <div id="sub-menu"><h4>Esqueci a Senha</h4></div>
+    <div id="titulo">Informe o Email cadastrado</div>
+    <div class="container">
+    <form action="#" method="post">
+    <div id="email">Email</div>
+    <textarea id="preco-produto" placeholder="johndoe@gmail.com" name="preco-produto" rows="2"></textarea><br><br>
+    <button type="submit">Recuperar Senha</button>
+    </form>
+    <img src="../../assets/img/esqueci-senha.png " alt="esqueci-senha" id="esqueci">
+    </div>
+</main>
+    <?php get_footer() ?>
+</body>
+<script src="../../assets/js/script.js"></script>
+</html>
