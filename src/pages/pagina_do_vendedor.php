cf7ba929e09318ce9b5c7da56c6f164290d261ba
Felipe Braiani <felipe.braianitech@gmail.com>
2025-02-05 11:05:16 -0400

    Adicionado features

--- src/pages/pagina_do_vendedor.php
+++ src/pages/pagina_do_vendedor.php
@@ -32,7 +32,7 @@ $appName = get_app_name();
                 <div class="row">
                     <div class="col-12 pb-4">
                         <div class="card">
-                            <div class="card-body justify-content-center">
+                            <div class="card-body justify-content-center ptb-2">
                                 <div class="col-2 justify-content-center">
                                     <div class="avatar">
                                         <img src="../assets/img/cliente/studio_center.svg" alt="" class="figure-img img-fluid">
@@ -49,7 +49,7 @@ $appName = get_app_name();
                                             <a href="mailto:studiocenterpy@gmail.com">studiocenterpy@gmail.com</a>
                                         </div>
                                     </div>
-                                    <div class="row pt-3">
+                                    <div class="row pt-2">
                                         <div class="col-12">
                                             <p>Av. 15 de Agosto, 506, Centro</p>
                                             <p>Campo Grande - MS</p>
@@ -58,21 +58,20 @@ $appName = get_app_name();
                                 </div>
                                 <div class="col-3">
                                     <div class="row">
-                                        <div class="col-12">
-                                            <p>Avaliações: </p>
+                                        <div class="col-12 text-right stars">
+                                            <p class="mr-1">Avaliações: </p>
+                                            <p class="card_estrela_texto estrela_30">★★★★★</p>
                                         </div>
                                     </div>
-                                    <div class="row justify-content-center">
-                                        <div class="col-2 whatsapp">
-                                            <img src="../assets/img/icons/whatsapp.svg" alt="">
-                                        </div>
-                                        <div class="col-10">
+                                    <div class="row">
+                                        <div class="col-12 text-right">
+                                            <img class="whatsapp" src="../assets/img/icons/whatsapp.svg" alt="">
                                             <a target="_blank" href="https://wa.me/556734377000">(67) 3437-7000</a>
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-12">
-                                            <img src="" alt="">
+                                        <div class="col-12 icone float-right">
+                                            <img src="../assets/img/icons/coracao.svg" alt="" class="icone_selecionado">
                                         </div>
                                     </div>
                                 </div>
