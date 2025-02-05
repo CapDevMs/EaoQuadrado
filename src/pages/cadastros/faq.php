bd350d9a638d836fded24078ce33eece51d4ef13
Gabriel Paganotti <gabriel54099536@aluno.ms.senac.br>
2025-02-05 08:27:37 -0400

    Pequenos ajustes na opacidade de alguns textos do faq

--- src/pages/cadastros/faq.php
+++ src/pages/cadastros/faq.php
@@ -16,28 +16,28 @@
     <main>
         <div id="menu">FAQ</div>
         <div id="sub-menu">FAQ</div>
-        <div id="texto">Perguntas Frequentes sobre o nosso E-commerce <br> <br>
+        <div id="texto">Perguntas Frequentes sobre o nosso E-commerce:<br> <br>
 
-            1 - Como faço para vender no site? <br> <br> 
-            • Para começar a vender, você precisa se cadastrar e passar por uma avaliação inicial. Novos vendedores são submetidos a uma avaliação durante suas primeiras vendas antes de se tornarem permanentes na plataforma.<br><br>
+            <span class = 'subtitulo'>1 - Como faço para vender no site?</span> <br> <br> 
+            <span class = 'texto'>• Para começar a vender, você precisa se cadastrar e passar por uma avaliação inicial. Novos vendedores são submetidos a uma avaliação durante suas primeiras vendas antes de se tornarem permanentes na plataforma.</span><br><br>
             
-            2 - Como funciona o processo de compra?<br><br>
-            • Todas as compras são intermediadas pelo nosso site. Após a confirmação do pedido ocorre direcionamento para cliente falar com vendedor e combinar entrega, após recebido o pedido o cliente podera avaliar sua compra.<br><br>
+            <span class = 'subtitulo'>2 - Como funciona o processo de compra?</span><br><br>
+            <span class = 'texto'>• Todas as compras são intermediadas pelo nosso site. Após a confirmação do pedido ocorre direcionamento para cliente falar com vendedor e combinar entrega, após recebido o pedido o cliente podera avaliar sua compra.</span><br><br>
             
-            3 - Como os vendedores adicionam produtos ao site?<br><br>
-            • Os vendedores têm acesso para listar e gerenciar seus produtos diretamente no site. Nosso sistema facilita a comunicação entre vendedores e clientes durante todo o processo de venda.<br><br>
+            <span class = 'subtitulo'>3 - Como os vendedores adicionam produtos ao site?</span><br><br>
+            <span>• Os vendedores têm acesso para listar e gerenciar seus produtos diretamente no site. Nosso sistema facilita a comunicação entre vendedores e clientes durante todo o processo de venda.</span><br><br>
             
-            4 - Existe um sistema de avaliações para os vendedores?<br><br>
-            • Sim, temos um sistema de avaliação onde os clientes podem avaliar os vendedores. Todas as avaliações são moderadas para garantir a qualidade dos serviços prestados.<br><br>
+            <span class = 'subtitulo'>4 - Existe um sistema de avaliações para os vendedores?</span><br><br>
+            <span class = 'texto'>• Sim, temos um sistema de avaliação onde os clientes podem avaliar os vendedores. Todas as avaliações são moderadas para garantir a qualidade dos serviços prestados.</span><br><br>
             
-            5 - Quais são as opções de entrega disponíveis?<br><br>
-            • Oferecemos entrega local com taxa fixa dentro de nossa área de serviço. Além disso, disponibilizamos opções para retirada na loja ou combinação de entrega direta entre vendedor e cliente.<br><br>
+            <span class = 'subtitulo'>5 - Quais são as opções de entrega disponíveis?</span><br><br>
+            <span class = 'texto'>• Oferecemos entrega local com taxa fixa dentro de nossa área de serviço. Além disso, disponibilizamos opções para retirada na loja ou combinação de entrega direta entre vendedor e cliente.</span><br><br>
             
-            6 - Quais métodos de pagamento são aceitos?<br><br>
-            • Os métodos de pagamento são a combinar com o vendedor. Isso proporciona flexibilidade para adaptar-se às necessidades de ambos os lados da transação.<br><br>
+            <span class = 'subtitulo'>6 - Quais métodos de pagamento são aceitos?</span><br><br>
+            <span class = 'texto'> Os métodos de pagamento são a combinar com o vendedor. Isso proporciona flexibilidade para adaptar-se às necessidades de ambos os lados da transação.</span><br><br>
             
-            7 - Como é garantida a segurança das transações?<br><br>
-            • Implementamos rigorosas medidas de segurança para proteger as transações e garantir a conformidade com nossos termos de serviço.<br><br>
+            <span class = 'subtitulo'>7 - Como é garantida a segurança das transações?</span><br><br>
+            <span class = 'texto'>• Implementamos rigorosas medidas de segurança para proteger as transações e garantir a conformidade com nossos termos de serviço.</span><br><br>
             
             </div>
 
