24ca8dfde53726bbf37e59b3673bfaf32369c7ea
Ricardo <sousasilvaricardo9@gmail.com>
2025-02-07 01:04:26 -0400

    configurando layout video

--- src/pages/categoria.php
+++ src/pages/categoria.php
@@ -1,126 +1,5 @@
 <?php
-<<<<<<< HEAD
-require_once('./config/funcoes.php');
-require('./config/conexao.php');
-
-?>
-<?php get_base_head(); ?>
-<body>
-    <header class="header">
-        <nav class="navbar">
-            <div class="navbar-logo">
-                <a href="#">
-                    <img src="./assets/img/logo.png" class="logo" alt="Logo">
-                </a>
-            </div>
-            <div class="search-bar">
-                <input type="text" class="search-input" placeholder="O que você procura hoje?">
-                <img src="./assets/img/pesquisar.svg" class="search-logo" alt="Pesquisar">
-            </div>
-            <ul class="menu-opcoes">
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/pessoa.svg" alt="Pessoa">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/carrinho.svg" alt="Carrinho">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/informacao.svg" alt="Informações">
-                    </a>
-                </li>
-                <li class="menu-opcoes-icons">
-                    <a href="#">
-                        <img src="./assets/img/suporte.svg" alt="suporte">
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-    <main>
-        <div class="container">
-            <h1>Conteúdo da página</h1>
-        </div>
-    </main>
-
-    <footer>
-        <div class="footer-links">
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Suporte</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Senac Hub Academy</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">eaoquadado@gmail.com</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">+55 67 99999-9999</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">
-                                R. do Parque, 75 - Centro Campo Grande - MS
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Conta</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Minha Conta</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Entre/Cadastre</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Carrinho</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Lista de Desejos</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="lista">
-                <div class="footer-links-title">
-                    <p>Acesso</p>
-                </div>
-                <div>
-                    <ul class="footer-links-list">
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Nossa Política</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Termos de Uso</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">FAQ</a>
-                        </li>
-                        <li class="footer-links-item">
-                            <a href="#" class="footer-links-link">Contato</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
-</body>
-<script src="assets/js/script.js"></script>
-=======
-    include('../config/funcoes.php');
+    include('../../config/funcoes.php');
     $appName = get_app_name();
 ?>
 <!DOCTYPE html>
@@ -180,22 +59,22 @@ require('./config/conexao.php');
         <section class="section-categoria">
             <div class="section-categoria__button">
                 <button class="categoria-celular">
-                    <img src="src\assets\img\CATEGORIA\ICONE_CATEGORIA\Category-CellPhone.png"id="Categoria-celular" alt="categoria-celular">
+                    <img src="../assets/img/categoria/icone-categoria/Category-CellPhone.png"id="Categoria-celular" alt="categoria-celular">
                 </button> 
                 <button class="categoria-computador">
-                    <img src="../assets/img/CATEGORIA/Categoria-computador.png" id="Categoria-computador" alt="Categoria-computador">
+                    <img src="../assets/img/categoria/icone-categoria/Category-Computer.png" id="Categoria-computador" alt="Categoria-computador">
                 </button>
                 <button class="categoria-smartwatche">   
-                    <img src="../assets/img/CATEGORIA/Categoria-smartwatche.png" id="Categoria-smartwatche" alt="Categoria-smartwache">
+                    <img src="../assets/img/categoria/icone-categoria/Category-SmartWatch.png" id="Categoria-smartwatche" alt="Categoria-smartwache">
                 </button>
                 <button class="categoria-camera">
-                    <img src="../assets/img/CATEGORIA/Categoria-camera.png" alt="Categoria-camera" id="Categoria-camera">
+                    <img src="../assets/img/categoria/icone-categoria/Category-Camera.png" alt="Categoria-camera" id="Categoria-camera">
                 </button>
                 <button class="categoria-fone-de-ouvido">    
-                    <img src="../assets/img/CATEGORIA/Categoria-fone_de_ouvido.png" alt="Categoria fone de ouvido" id="Categoria-fone_de_ouvido">
+                    <img src="../assets/img/categoria/icone-categoria/Category-Headphone.png" alt="Categoria fone de ouvido" id="Categoria-fone_de_ouvido">
                 </button>
                 <button class="categoria-gamer">    
-                    <img src="../assets/img/CATEGORIA/Categoria-gamer.png"alt="categoria gamer" id="Categoria-gamer">
+                    <img src="../assets/img/categoria/Category-Gamepad.png"alt="categoria gamer" id="Categoria-gamer">
                 </button>    
             </div>
         </section>
@@ -205,5 +84,4 @@ require('./config/conexao.php');
     <?php get_footer() ?>
 </body>
 <script src="../assets/js/script.js"></script>
->>>>>>> c034c0e48908badefc9cb30ae34bacbcc36f2b1f
 </html>
