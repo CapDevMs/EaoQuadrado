cd4f65d4f9ae47bf1a7b45b04094fd8488157251
Ricardo <sousasilvaricardo9@gmail.com>
2025-02-10 07:00:04 -0400

    atualizando branch

--- src/pages/categoria.php
+++ src/pages/categoria.php
@@ -14,7 +14,7 @@
     <?php get_header() ?>
 
     <div class ="sidebar">
-        <div class="marcas_preco">
+        <div class="sidebar__marcas-preco">
                 <h1 class="marcas">Marcas</h1>
                 <input class="search-bar-marcas" placeholder="Buscar Produtos">
                 <button class="search-marca" alt="Pesquisar_Marca"></button>
@@ -41,44 +41,44 @@
             
             <input type="checkbox" name="tudo"><label for="tudo">Tudo</label>
             <input type="checkbox" name="5-star"><label for="5-star" id="star5"><img src="../assets/img/CATEGORIA/Five_Star.png" id="5_star" alt=""></label>
-            <input type="checkbox" name="5-star"><label for="5-star" id="star4"><img src="../assets/img/CATEGORIA/Four_Star.png" id="5_star" alt=""></label>
-            <input type="checkbox" name="5-star"><label for="5-star" id="star3"><img src="../assets/img/CATEGORIA/Three_Star.png" id="5_star" alt=""></label>
-            <input type="checkbox" name="5-star"><label for="5-star" id="star2"><img src="../assets/img/CATEGORIA/Two_Star.png" id="5_star" alt=""></label>
-            <input type="checkbox" name="5-star"><label for="5-star" id="star1"><img src="../assets/img/CATEGORIA/One_Star.png" id="5_star" alt=""></label>
+            <input type="checkbox" name="4-star"><label for="4-star" id="star4"><img src="../assets/img/CATEGORIA/Four_Star.png" id="4_star" alt=""></label>
+            <input type="checkbox" name="3-star"><label for="3-star" id="star3"><img src="../assets/img/CATEGORIA/Three_Star.png" id="3_star" alt=""></label>
+            <input type="checkbox" name="2-star"><label for="2-star" id="star2"><img src="../assets/img/CATEGORIA/Two_Star.png" id="2_star" alt=""></label>
+            <input type="checkbox" name="1-star"><label for="1-star" id="star1"><img src="../assets/img/CATEGORIA/One_Star.png" id="1_star" alt=""></label>
         </div>
     </div>
     <main>
-
-        <div class="container-video">
-            <div class="video-celular">
-            <video autoplay muted loop>
-                <source src="../assets/videos/video-categoria_celular.mp4.mp4" type="video/mp4">        
-            </video>
-            </div>      
-        </div> 
-        <section class="section-categoria">
-            <div class="section-categoria__button">
-                <button class="categoria-celular">
-                    <img src="src\assets\img\CATEGORIA\ICONE_CATEGORIA\Category-CellPhone.png"id="Categoria-celular" alt="categoria-celular">
-                </button> 
-                <button class="categoria-computador">
-                    <img src="../assets/img/CATEGORIA/Categoria-computador.png" id="Categoria-computador" alt="Categoria-computador">
-                </button>
-                <button class="categoria-smartwatche">   
-                    <img src="../assets/img/CATEGORIA/Categoria-smartwatche.png" id="Categoria-smartwatche" alt="Categoria-smartwache">
-                </button>
-                <button class="categoria-camera">
-                    <img src="../assets/img/CATEGORIA/Categoria-camera.png" alt="Categoria-camera" id="Categoria-camera">
-                </button>
-                <button class="categoria-fone-de-ouvido">    
-                    <img src="../assets/img/CATEGORIA/Categoria-fone_de_ouvido.png" alt="Categoria fone de ouvido" id="Categoria-fone_de_ouvido">
-                </button>
-                <button class="categoria-gamer">    
-                    <img src="../assets/img/CATEGORIA/Categoria-gamer.png"alt="categoria gamer" id="Categoria-gamer">
-                </button>    
-            </div>
-        </section>
-                       
+        <div class="main-content">
+            <div class="container-video">
+                <div class="container-video__video-celular">
+                <video autoplay muted loop>
+                    <source src="../assets/videos/video-categoria_celular.mp4.mp4" type="video/mp4">        
+                </video>
+                </div>      
+            </div> 
+            <section class="section-categoria">
+                <div class="section-categoria__button">
+                    <button class="categoria-celular">
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-CellPhone.png"id="Categoria-celular" alt="categoria-celular">
+                    </button> 
+                    <button class="categoria-computador">
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-Computer.png" id="Categoria-computador" alt="Categoria-computador">
+                    </button>
+                    <button class="categoria-smartwatche">   
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-SmartWatch.png" id="Categoria-smartwatche" alt="Categoria-smartwache">
+                    </button>
+                    <button class="categoria-camera">
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-Camera.png"  id="Categoria-camera" alt="Categoria-camera">
+                    </button>
+                    <button class="categoria-fone-de-ouvido">    
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-Headphone.png" alt="categoria fone de ouvido" id="Categoria-fone_de_ouvido">
+                    </button>
+                    <button class="categoria-gamer">    
+                        <img src="../assets/img/categoria/img-produtos/icones-categoria/Category-Gamepad.png"alt="categoria gamer" id="Categoria-gamer">
+                    </button>    
+                </div>
+            </section>
+        </div>               
     </main>
 
     <?php get_footer() ?>
