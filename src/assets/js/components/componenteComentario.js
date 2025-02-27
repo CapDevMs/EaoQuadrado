8ac010c14f03d00e15ecc3a7bebb97420bebfd59
annalorena22 <annalorena1998@gmail.com>
2025-02-27 10:16:15 -0400

    iniciando componente comentario

---
+++ src/assets/js/components/componenteComentario.js
@@ -0,0 +1,15 @@
+const componenteComentario = ({ cliente, comentario }) => {
+    return 
+    `<div class="container-comentarios">
+        <div class="comentario">
+            <img src="../../assets/img/icone_perfil.png" class="foto" alt="alguem">
+            <h1>${cliente}</h1>
+            <img src="../../assets/img/Four Star.png" class="estrelinha4" alt="estrela4">
+            <p>${comentario}</p>
+        </div>
+    </div>
+    `
+
+}
+
+export default componenteComentario;
