3d38c7366d326ee141cc3022ef3cdf75916b882a
Ricardo <sousasilvaricardo9@gmail.com>
2025-03-06 09:31:35 -0400

    vai da tudo certo

--- src/assets/js/categoria.js
+++ src/assets/js/categoria.js
@@ -1,42 +1,33 @@
-    let buttonCategoria = [
-        {           
-            'nome': 'Computadores',
-            'imagem': '../assets/img/categoria/icone-categoria/computer_icon.svg'
-        },
-        {           
-            'nome': 'Celulares',
-            'imagem': '../assets/img/categoria/icone-categoria/cellphone_icon.svg'
-        },
-        {           
-            'nome': 'Games',
-            'imagem': '../assets/img/categoria/icone-categoria/games_icon.svg'
-        },
-        {           
-            'nome': 'Fone de ouvido',
-            'imagem': '../assets/img/categoria/icone-categoria/headphone_icon.svg'
-        },
-        {           
-            'nome': 'Camera',
-            'imagem': '../assets/img/categoria/icone-categoria/camera_icon.svg'
-        },
-        {           
-            'nome': 'Smartwatches',
-            'imagem': '../assets/img/categoria/icone-categoria/smartwaches_icon.svg'
-        },
-        
-    ]
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
+    const categorias = [
+        { nome: "Computadores", imagem: "../assets/img/categoria/icone-categoria/computer_icon.svg" },
+        { nome: "Celulares", imagem: "../assets/img/categoria/icone-categoria/cellphone_icon.svg" },
+        { nome: "Games", imagem: "../assets/img/categoria/icone-categoria/games_icon.svg" },
+        { nome: "Fone de ouvido", imagem: "../assets/img/categoria/icone-categoria/headphone_icon.svg" },
+        { nome: "Camera", imagem: "../assets/img/categoria/icone-categoria/camera_icon.svg" },
+        { nome: "Smartwatches", imagem: "../assets/img/categoria/icone-categoria/smartwaches_icon.svg" }
+    ];
+
+    // Elemento onde os botões serão inseridos
+    const containerCategorias = document.getElementById("categorias");
+
+    // Criar os botões dinamicamente
+    categorias.forEach(categoria => {
+        const botao = document.createElement("button-categoria");
+        botao.setAttribute("nome", categoria.nome);
+        botao.setAttribute("imagem", categoria.imagem);
+        containerCategorias.appendChild(botao);
+    });
+
+    // Lidar com o clique nos botões e redirecionamento
     document.querySelectorAll(".buttonCategoria").forEach(button => {
         button.addEventListener("click", (event) => {
             const categoria = event.currentTarget.getAttribute("data-categoria");
-            
             window.location.href = `?categoria=${encodeURIComponent(categoria)}`;
         });
     });
 
+    // Capturar parâmetro da URL e carregar produtos
     const params = new URLSearchParams(window.location.search);
     const categoriaSelecionada = params.get("categoria");
 
@@ -45,8 +36,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-
 function carregarProdutos(categoria) {
     console.log(`Carregando produtos para a categoria: ${categoria}`);
-    
 }
--- src/assets/js/components/button-categoria.js
+++ src/assets/js/components/button-categoria.js
@@ -1,19 +1,26 @@
-const buttonCategoria = ({ imagem, nome }) => {
-    return `
-    <div class="container">
-        <button class="buttonCategoria">
-            <div class="button-body">
-                <div class="container-imagem">
-                    <img class="img-produto" src="${imagem}" alt="Ícone de ${nome}">
+class ButtonCategoria extends HTMLElement {
+    connectedCallback() {
+        const nome = this.getAttribute("nome");
+        const imagem = this.getAttribute("imagem");
+
+        this.innerHTML = `
+        <div class="container">
+            <button class="buttonCategoria" data-categoria="${nome}">
+                <div class="button-body">
+                    <div class="container-imagem">
+                        <img class="img-produto" src="${imagem}" alt="Ícone de ${nome}">
+                    </div>
+                    <div class="container-nome-produto">
+                        <h3 class="nome-produto">${nome}</h3>
+                    </div>
                 </div>
-                <div class="container-nome-produto">
-                    <h3 class="nome-produto">${nome}</h3>
-                </div>
-            </div>
-        </button>
-    </div>
-    `;
+            </button>
+        </div>
+        `;
+    }
 }
+
+// Registrar o componente
 customElements.define("button-categoria", ButtonCategoria);
 
-export default buttonCategoria;
+export default ButtonCategoria;
