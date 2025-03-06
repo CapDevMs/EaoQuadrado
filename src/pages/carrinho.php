25a80764b5568f5f09be395d8219be1ae67f7c57
Gabriel Paganotti <gabriel54099536@aluno.ms.senac.br>
2025-03-06 10:55:56 -0400

    outsider

--- src/pages/carrinho.php
+++ src/pages/carrinho.php
@@ -3,11 +3,14 @@ require_once('../config/funcoes.php');
 require('../config/conexao.php');
 
 ?>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
 <?php get_base_head(); ?>
 
 <body>
     <?php get_header() ?>
     <link rel='stylesheet' href="../assets/css/carrinho_cliente/style.css">
+    <link rel='stylesheet' href="../assets/css/base.css ">
 
 
 
@@ -42,10 +45,14 @@ require('../config/conexao.php');
                     
                     </div>
 
-                    <div class='productDiv'>
-                    
+                    <div class='productDiv counterDiv'>
+                        <div>
                         <span class='product-1-1'>Quantidade</span>
+                        </div>
+
+                        <div>
                         <input type="Number" class='counter' min='0' value = 1>
+                        </div>
                     
                     </div>
 
@@ -56,7 +63,57 @@ require('../config/conexao.php');
                     
                     </div>
 
-                    <div class='cartProduct-6'>
+                    <div class='productDiv cartProduct-6'>
+                    
+                        <a href='#' class = 'a'><img src='../assets/img/lixeira-icon.png' class='icon removeProductButton'></a>
+                    
+                    </div>
+
+                </div>
+
+
+
+                <div class='product'>
+
+                    <div class='productDiv'>
+
+                        <img src='../assets/img/acer_nitro.png' class='image '>
+                    
+                    </div>  
+
+                    <div class='productDiv'>
+
+                        <span class=''>Nome do produto</span>
+                        <span class=''>Acer nitro V5</span>
+                    
+                    </div>
+
+                    <div class='productDiv'>
+                    
+                        <span class=''>Preço</span>
+                        <span class='price'>R$ 4690,00</span>
+                    
+                    </div>
+
+                    <div class='productDiv counterDiv'>
+                        <div>
+                        <span class='product-1-1'>Quantidade</span>
+                        </div>
+
+                        <div>
+                        <input type="Number" class='counter' min='0' value = 1>
+                        </div>
+                    
+                    </div>
+
+                    <div class='productDiv'>
+                    
+                        <span class=''>Subtotal</span>
+                        <span class='subTotal'>...</span>
+                    
+                    </div>
+
+                    <div class='productDiv cartProduct-6'>
                     
                         <a href='#' class = 'a'><img src='../assets/img/lixeira-icon.png' class='icon removeProductButton'></a>
                     
@@ -67,8 +124,24 @@ require('../config/conexao.php');
 
             </div>
         </div>
-    </main>
 
+    </main>
+    
+    
+            <div class="totalizador">
+                <div class="totalLine">
+                    <span class = 'span'>Subtotal</span>
+                    <span class="span">...</span>
+                </div><hr>
+                <div class="totalLine">
+                    <span class = 'span'>Frete:</span>
+                    <span class="span">A combinar</span>
+                </div><hr>
+                <div class="totalLine">
+                    <span class = 'span'>Total:</span>
+                    <span class="span">...</span>
+                </div>
+            </div>
     <?php get_footer() ?>
     <script src="../../src/assets/js/script.js"></script>
     <script src="../../src/assets/js/carrinho/script.js"></script>
