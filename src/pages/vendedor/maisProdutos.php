76ed19baf409d6e8975a2e65d7b63b1c6ec3a208
Gabriel Paganotti <gabriel54099536@aluno.ms.senac.br>
2025-03-13 11:06:22 -0400

    ...

---
+++ src/pages/vendedor/maisProdutos.php
@@ -0,0 +1,27 @@
+<?php
+require_once('../../config/funcoes.php');
+require('../../config/conexao.php');
+
+
+?>
+<?php get_base_head(); ?>
+<link rel="stylesheet" href="../assets/css/base.css">
+<link rel="stylesheet" href="../assets/css/base.css">
+<body>
+    <?php get_header()?>
+   
+    <main>
+        <div class="container">
+            <h1>Conteúdo da página</h1>
+            <img src="../../assets/img/Banner_loja.png" alt="bannerLoja">
+            <img src="../../assets/img/fotoProdutos/ração.png" alt="produto">
+            <img src="../../assets/img/fotoProdutos/camera.png" alt="produto">
+            <img src="../../assets/img/fotoProdutos/asus_notebook.png" alt="produto">
+            <img src="../../assets/img/fotoProdutos/curologiProductSet.png" alt="produto">
+        </div>
+    </main>
+
+    <?php get_footer()?>
+</body>
+<script src="assets/js/script.js"></script>
+</html>
