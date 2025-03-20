19edf6d1d79381ab754b8cb4cd079a7c1fc53cf7
unknown <rafasmontiel13@gmail.com>
2025-03-20 05:34:25 -0400

    cadastro pronto

--- src/pages/admin/cadastro_adm.php
+++ src/pages/admin/cadastro_adm.php
@@ -1,15 +1,23 @@
-<?php
-require_once('../../config/funcoes.php');
-require('../../config/conexao.php');
+<?php 
+    include('../config/funcoes.php');
 ?>
-<?php get_base_head(); ?>
 
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php get_css(['cadastro_adm', 'base', 'style']) ?>
+    
+    <title>Eao Quadrado</title>
+</head>
 <body>
-    <?php get_header(); ?>
-    <link rel = 'stylesheet' href="../../assets/css/cadastro_adm.css">
-    <link rel = 'stylesheet' href="../../assets/css/base.css">
-
+    <header class="header">
+    <?php get_header() ?>
+    </header>
 <main>
+
     <div class="roadmap">Home / Painel do Administrador / Cadastrar Novo Administrador</div>
             <div class="container-geral">
                 <div class="menu-container">
@@ -29,80 +37,94 @@ require('../../config/conexao.php');
                     <a href="#">Criar Categoria</a>
                     <a href="#">Editar Sistema</a>
                 </div>
-                
-                <form  action="#">
+            
+                <form action="#">
                     <div class="register_client">
-                        <h1>Cadastro de Administrador</h1>
+                        <h2>Cadastro de Administrador</h2>
                         <img src="../../assets/img/foto_cliente.png" alt="foto do cliente">
-        
-                        <div class="names-container">
-                            <h2>Nome</h2>
-                            <input type="text" name="Nome" class="input1-container" placeholder="John" required>
-                            <h2>Sobrenome</h2>
-                            <input type="text" name="Sobrenome" class="input1-container" placeholder="Doe" required>
-                        </div>
-        
-                        <h2 class="subtitle">Data de Nascimento</h2>
-                        <div class="date-container">
-                            <div class="counter">
-                                <input type="date" class="dia-mes" max="31" value="1" required></input>
-                            </div>
-                        </div>
-        
                         
-        
-                        <div class="inform1-container">
-                            <h2>CPF</h2>
-                            <input type="text" name="cpf" class="input2-container" placeholder="000.000.000-00" required>
-                            <h2>E-mail</h2>
-                            <input type="text" name="email" class="input2-container" placeholder="johndoe@gmail.com" required>
-                            <h2>Número de Telefone</h2>
-                            <input type="text" name="telefone" class="input2-container" placeholder="(67) 99999-9999" required>
-                        </div>
-        
-        
-                    <div class="terms-container">
-                            <h1>Permissões:</h1>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="termos" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_termos">Validar Novas Contas de Vendedores</label>
+                        <div class="container-inter">
+                            <div class="inform-container">
+
+                                <div class="nome-group">
+                                    <label for="nome">Nome</label>
+                                    <input type="text" name="Nome" id="nome" class="input1" placeholder="John" required>
+                                </div>
+
+                                <div class="sobrenome-group">
+                                    <label for="sobrenome">Sobrenome</label>
+                                    <input type="text" name="Sobrenome" id="sobrenome" class="input1" placeholder="Doe" required>
+                                </div>
+
+                                <div class="date-group">
+                                    <label for="nascimento">Data de Nascimento</label>
+                                    <input type="date" id="nascimento" name="nascimento" required>
+                                </div>                            
+                                
+                                <div class="cpf-group">
+                                    <label for="cpf">CPF</label>
+                                    <input type="text" name="cpf" id="cpf" class="input1" placeholder="000.000.000-00" required>
+                                </div>
+
+                                <div class="email-group">
+                                    <label for="email">E-mail</label>
+                                    <input type="text" name="email" id="email" class="input1" placeholder="johndoe@gmail.com" required>
+                                </div>
+
+                                <div class="cell-group">
+                                    <label for="telefone">Número de Telefone</label>
+                                    <input type="text" name="telefone" id="telefone" class="input1" placeholder="(67) 99999-9999" required>
+                                </div> 
+
                             </div>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_politica">Suporte ao Cliente</label>
+
+                            
+                            <div class="inform-container2">
+                                <div class="terms-container">
+                                    <h1>Permissões:</h1>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="termos" class="checkbox" id="confirmCheckbox1" required>
+                                        <label for="confirmCheckbox1">Validar Novas Contas de Vendedores</label>
+                                    </div>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox2" required>
+                                        <label for="confirmCheckbox2">Suporte ao Cliente</label>
+                                    </div>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox3" required>
+                                        <label for="confirmCheckbox3">Suporte ao Vendedor</label>
+                                    </div>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox4" required>
+                                        <label for="confirmCheckbox4">Desativar Vendedor</label>
+                                    </div>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox5" required>
+                                        <label for="confirmCheckbox5">Criar Anúncio</label>
+                                    </div>
+                                    <div class="checkbox-container">
+                                        <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox6" required>
+                                        <label for="confirmCheckbox6">Criar Categoria</label>
+                                    </div>
+                                </div>
+
+                                <div class="senha-group">
+                                    <label for="senha">Senha</label>
+                                    <input type="password" id="senha" name="senha" class="input1" placeholder="*********" required>
+                                </div>
+
+                                <div class="confirm-group">
+                                    <label for="confirmar-senha">Confirmar Senha</label>
+                                    <input type="password" id="confirmar-senha" name="confirmar-senha" class="input1" placeholder="*********" required>
+                                </div>
                             </div>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_politica">Suporte ao Vendedor</label>
-                            </div>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_politica">Desativar Vendedor</label>
-                            </div>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_politica">Criar Anúncio</label>
-                            </div>
-                            <div class="checkbox-container">
-                                <input type="checkbox" name="politicas" class="checkbox" id="confirmCheckbox" required>
-                                <label for="confirm_politica">Criar Categoria</label>
-                            </div>
-                        </div>
-        
-        
-                        <div class="password-container">
-                            <h2>Senha</h2>
-                            <input type="text" class="input3-container" placeholder="*********" required>
-                            <h2>Confirmar Senha</h2>
-                            <input type="text" class="input3-container" placeholder="*********" required>
-                        </div>
-        
-        
-                    </div>  
+                        </div>  
+                    </div>
+
                     <button type="submit" class="button-confirm">Registrar</button>
                 </form>
             </div>
-                 
+            
 </main>
 
     <?php get_footer()?>    
