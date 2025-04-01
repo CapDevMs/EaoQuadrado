d2f246b03b589ff41e596774014b36843b9e036d
rafavgtt <rafavergottii@gmail.com>
2025-04-01 09:37:14 -0400

    fix(adm): Cores Variaveis e checkbox

--- src/assets/js/components/permissoes-adm.js
+++ src/assets/js/components/permissoes-adm.js
@@ -1,7 +1,7 @@
 const permissoesAdm = ({ nome, valor, nomePermissao}) => {
     return `
         <div class="container-permissoes">
-            <input type="radio" class="input-radio" name="${nome} value="${valor}">
+            <input type="checkbox" class="input-radio" name="${nome} value="${valor}">
             <label for="${valor}">${nomePermissao}</label>
         </div>
     <br>
