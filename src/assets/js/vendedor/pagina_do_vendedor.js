5cea8e136b7d6856273c625ada945809326caff6
annalorena22 <annalorena1998@gmail.com>
2025-04-02 11:02:08 -0400

    Css do componente quase finalizado. Falta terminar a responsividade

--- src/assets/js/vendedor/pagina_do_vendedor.js
+++ src/assets/js/vendedor/pagina_do_vendedor.js
@@ -4,12 +4,12 @@ let comentario = document.querySelector("container-comentarios");
 
 let comentarios = [
         {
-        'cliente' : "Anna",
-        'comentario': "Top!!"
+        'cliente' : "Alcir Júnior",
+        'comentario': "Chegou tudo certinho, dentro do prazo, a embalagem intacta, o produto com todas as proteções plásticos, isopor.... funciona perfeitamente já fiz o teste e é ótima."
     },
     {
-        'cliente' : "João",
-        'comentario': "Massa!!"
+        'cliente' : "Andrey Silva",
+        'comentario': "Comprei uma chuteira muito boa. Excelente! Podem comprar sem medo. Jogo bola a minha vida inteira e posso afirmar que a chuteira é boa. Não sei a durabilidade, só o tempo vai dizer, mas fica muito boa no pé."
     }
 ]
 
--- src/pages/vendedor/pagina_do_vendedor.php
+++ src/pages/vendedor/pagina_do_vendedor.php
@@ -8,11 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../assets/css/base.css">
-    <link rel="stylesheet" href="../../assets/css/vendedor/pagina_do_vendedor.css">
     <link rel="shortcut icon" href="../../assets/img/logo.png">
-    <?php get_css(['vendedor/pagina_do_vendedor.css', 'style.css']) ?>
+    <?php get_css(['vendedor/pagina_do_vendedor', 'style', 'components/comentario_avaliacao']) ?>
 
     <title>E ao Quadrado</title>
 </head>
