258c1e3e69b842675f4668347e9523781dc3eef1
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2025-04-02 08:55:17 -0400

    adicionado nome aba

--- src/pages/cadastros/sobre.php
+++ src/pages/cadastros/sobre.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
+    <title>Sobre</title>
     <?php get_css(['cadastros/sobre']) ?>
 </head>
 <body>
