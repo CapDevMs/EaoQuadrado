73a09545dcbc2d4ac29c058e896f72c3babf3d39
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2025-04-02 08:50:55 -0400

    atualizado nome aba

--- src/pages/admin/abertura-de-chamados.php
+++ src/pages/admin/abertura-de-chamados.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
+    <title>Abertura de Chamados - ADM</title>
     <?php get_css(['admin/abertura-de-chamados']) ?>
 </head>
 <body>
