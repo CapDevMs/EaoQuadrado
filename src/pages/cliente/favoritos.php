60ee553f8a3e470d825613eafa89e2e3ccb9c165
Ricardo <sousasilvaricardo9@gmail.com>
2025-04-02 09:17:27 -0400

    alterando mais nomes de classes

--- src/pages/cliente/favoritos.php
+++ src/pages/cliente/favoritos.php
@@ -9,20 +9,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <title><?= $appName . ' - ' . $titulo ?? '' ?></title> -->
-    <title>E ao Quadrado - O melhor E-commerce</title>
+    <title>E ao Quadrado - Favoritos</title>
     <?php get_css(['cliente/favoritos_cliente']) ?>
     <?php get_css_components() ?>
 </head>
 <body>
+
     <?php get_header() ?>
 
     <main>
         <div class="menu-lateral">
-            <div class="menuzin">
+            <div class="roadmap">
                 <div>
                     <ul class="mi-cont">
                         <li class="mi-cont">
-                            <a href="#" class="mi-cont">Home</a>
+                            <a href="../index.php" class="mi-cont">Home</a>
                         </li>
                         <li class="mi-cont">
                             <p class="mi-cont">/</p>
@@ -37,7 +38,7 @@
             <div class="container-principal">
                 <div class="fav-tittle">
                     <h4 class="title">Produtos Favoritos</h4>
-                    <a href="#"><button class="but1">Mover tudo para o carrinho</button></a>
+                    <a href="#"><button class="button-mover-TudoCarrinho">Mover tudo para o carrinho</button></a>
                 </div>
                 <div class="produto-favorito">
                     <div class="container">
@@ -48,18 +49,18 @@
                         <a href="#">
                             <button class="but2">Adicione ao carrinho</button>
                         </a>
-                            <h4 class="name-prod">ASUS Notebook Gamer</h4>
+                            <h4 class="nome-produto">ASUS Notebook Gamer</h4>
                             <h5 class="preco-prod">R$960</h5>
                     </div>
                     <div class="container">
                         <img class="edit-favorito" src="../../assets/img/computador.png">
                         <a href="#">
-                            <img class="but-car" src="../../assets/img/carrinho2.svg">
+                            <img class="button-carrinho" src="../../assets/img/carrinho2.svg">
                         </a>
                         <a href="#">
                             <button class="but2">Adicione ao carrinho</button>
                         </a>
-                        <h4 class="name-prod">ASUS Notebook Gamer</h4>
+                        <h4 class="nome-produto">ASUS Notebook Gamer</h4>
                         <h5 class="preco-prod">R$960</h5>
                     </div>
                     <div class="container">
@@ -70,7 +71,7 @@
                         <a href="#">
                             <button class="but2">Adicione ao carrinho</button>
                         </a>
-                        <h4 class="name-prod">ASUS Notebook Gamer</h4>
+                        <h4 class="nome-produto">ASUS Notebook Gamer</h4>
                         <h5 class="preco-prod">R$960</h5>
                     </div>
                     <div class="container">
@@ -79,10 +80,10 @@
                             <img class="but-car" src="../../assets/img/carrinho2.svg">
                         </a>
                         <a href="#">
-                            <button class="but2">Adicione ao carrinho</button>
+                            <button class="button2">Adicione ao carrinho</button>
                         </a>
-                        <h4 class="name-prod">ASUS Notebook Gamer</h4>
-                        <h5 class="preco-prod">R$960</h5>
+                        <h4 class="nome-produto">ASUS Notebook Gamer</h4>
+                        <h5 class="preco-produto">R$960</h5>
                     </div>
                     
                 </div>
@@ -95,28 +96,28 @@
                         <a href="#">
                             <button class="but2">Visitar loja</button>
                         </a>
-                        <h4 class="name-prod">Studio Center</h4>
+                        <h4 class="nome-produto">Studio Center</h4>
                     </div>
                     <div class="container">
                         <img class="edit-loja-favorito" src="../../assets/img/loja-favorita.png">
                         <a href="#">
                             <button class="but2">Visitar loja</button>
                         </a>
-                        <h4 class="name-prod">Studio Center</h4>
+                        <h4 class="nome-produto">Studio Center</h4>
                     </div>
                     <div class="container">
                         <img class="edit-loja-favorito" src="../../assets/img/loja-favorita.png">
                         <a href="#">
                             <button class="but2">Visitar loja</button>
                         </a>
-                        <h4 class="name-prod">Studio Center</h4>
+                        <h4 class="nome-produto">Studio Center</h4>
                     </div>
                     <div class="container">
                         <img class="edit-loja-favorito" src="../../assets/img/loja-favorita.png">
                         <a href="#">
                             <button class="but2">Visitar loja</button>
                         </a>
-                        <h4 class="name-prod">Studio Center</h4>
+                        <h4 class="nome-produto">Studio Center</h4>
                     </div>
                     
                 </div>
