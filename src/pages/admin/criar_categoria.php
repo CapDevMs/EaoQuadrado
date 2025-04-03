4341644b096c49ba8e284e13cd9faf719438143b
unknown <rafasmontiel13@gmail.com>
2025-04-02 21:31:58 -0400

    ajustes

--- src/pages/admin/criar_categoria.php
+++ src/pages/admin/criar_categoria.php
@@ -1,16 +1,47 @@
 <?php
 require_once('../../config/funcoes.php');
-require('../../config/conexao.php');
-
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../assets/css/admin/criar_categoria.css">
+    <?php get_css(['base', 'criar_categoria' , 'style']) ?>
+    
+    <title>Criar Categoria</title>
+</head>
 <?php get_base_head(); ?>
 
 <body>
+    <header>
     <?php get_header(); ?>
-    
+    </header>
 
     <main>
+    <div class="roadmap">Home / Painel do Administrador / Cadastrar Novo Administrador</div>
+        <div class="container-geral">
+            <div class="menu-container">
+                <h1>Cadastro</h1>
+                <a href="#" id="pagina_atual">Cadastrar Novo Administrador</a>
+                <a href="./meu_perfil_adm.php">Gerenciar Meu Perfil</a>
+                <h1>Colaboradores</h1>
+                <a href="#">Validar Novo Colaborador</a>
+                <a href="#">Colaboradores Aprovados</a>
+                <a href="#">Colaboradores Reprovados</a>
+                <a href="#">Listar Colaboradores Ativos</a>
+                <a href="#">Suporte ao Colaborador</a>
+                <h1>Clientes</h1>
+                <a href="#">Suporte ao Cliente</a>
+                <h1>Sistema</h1>
+                <a href="#">Abrir Chamado</a>
+                <a href="#">Criar Categoria</a>
+                <a href="#">Editar Sistema</a>
+            </div>
 
+            div.
+        </div>
 
     </main>
 
