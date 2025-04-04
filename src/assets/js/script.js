83f63661b9ba4cdaa191e13ce9f2cbee30f77719
rafavgtt <rafavergottii@gmail.com>
2025-04-04 10:46:27 -0400

    menu header

---
+++ src/assets/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburguer = document.getElementById("hamburguer");
+  const menu = document.getElementById("mobileMenu");
+
+  hamburguer.addEventListener("click", () => {
+    if (menu.classList.contains("open")) {
+      menu.classList.remove("open");
+      menu.classList.add("close");
+      menu.addEventListener("animationend", () => {
+        menu.classList.remove("close");
+      });
+    } else {
+      menu.classList.add("open");
+    }
+  });
+});
--- src/components/header.php
+++ src/components/header.php
@@ -36,10 +36,19 @@ $base_url = get_base_url();
             </li>
         </ul>
         <div class="header-mobile">
-            <a href="">
-                <img src="<?=$base_url;?>/assets/img/menu-mobile.svg" alt="Menu">
-            </a>
+                <img src="<?=$base_url;?>/assets/img/menu-mobile.svg" alt="Menu" id="hamburguer">
 
         </div>
     </nav>
 </header>
+<!-- Menu Mobile -->
+<div class="mobile-menu" id="mobileMenu">
+    <ul>
+        <li><a href="<?=$base_url;?>/pages/login.php">Login</a></li>
+        <li><a href="<?=$base_url;?>/pages/carrinho.php">Carrinho</a></li>
+        <li><a href="<?=$base_url;?>/pages/cadastros/sobre.php">Sobre</a></li>
+        <li><a href="<?=$base_url;?>/pages/cadastros/contato.php">Contato</a></li>
+    </ul>
+</div>
+
+<script src="../assets/js/script.js"></script>
