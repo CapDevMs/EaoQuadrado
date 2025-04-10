f3c9586bea93a59364252e75dd1ed5218b636939
Vatreax <rafasmontiel13@gmail.com>
2025-04-10 11:02:50 -0400

    framework

--- src/pages/cadastros/contato.php
+++ src/pages/cadastros/contato.php
@@ -8,63 +8,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $appName . ' - ' . $titulo ?? '' ?></title>
-    <?php get_css(['cadastros/contato']) ?>
+    <?php get_css(['style', 'cadastros/contato']) ?>
 </head>
 <body>
     <?php get_header() ?>
 
     <main>
-        <a id="menu" href="index.php">Pagina Inicial</a>
-        <span>/</span>
-        <div id="sub-menu"><a href="contato.php">Contato</a></div>     
+        <!-- roadmap -->
 
-        <div class="container_geral row">
+        <div class="container pt-3">
+            <div class="row">
+                <div class="col-sm-12 col-md-3 pt-1">
+                    <div class="contatos-title pb-1">
+                        <span class="round"></span>
+                        <h1>Entre em Contato</h1>
+                    </div>
+                    <p class="pb-1">Atendimento 24 horas</p>
+                    <p class="pb-1">Telefone: +67991377273</p>
+                    <hr>
+                    <div class="contatos-title pb-1 pt-1">
+                        <span class="round"></span>
+                        <h1>Mande um Email</h1>
+                    </div>
+                    <p class="pb-1">Respondemos e-mail em 24 horas</p>
+                    <p class="pb-1">Email: customer@exclusive.com</p>
+                    <p>Email: support@exclusive.com</p>
+                </div>
 
-            <div class="contatos-container">
-                <div class="contatos-title">
-                    <hr class="round">
-                    <h1>Entre em Contato</h1>
-                    
-                </div>
-                <p>Atendimento 24 horas</p>
-                <p>Telefone: +67991377273</p>
-                <hr>
-                <div class="contatos-title">
-                    <hr class="round">
-                    <h1>Mande um Email</h1>
-                </div>
-                <p>Respondemos e-mail em 24 horas</p>
-                <p>Email: customer@exclusive.com</p>
-                <p>Email: support@exclusive.com</p>
+                
+                <form action="#" class="col-sm-12 col-md-7" method="post">
+
+                    <div class="caixa-container row">
+                        
+                        <div class="input-group col-md3 pb-2">
+                            <div class="titulo">
+                                <label for="nome">Nome</label>
+                                <input class="caixa" placeholder="John Doe" id="nome" ></input>
+                            </div>
+                            <div class="titulo">
+                                <label for="email">E-mail</label>
+                                <input class="caixa" id="email" placeholder="johndoe@gmail.com"></input>
+                            </div>
+                            <div class="titulo">
+                                <label for="telefone">Telefone/WhatsApp</label>
+                                <input class="caixa" id="telefone" placeholder="(67) 99999-9999"></input>
+                            </div>
+                        </div>
+
+                        <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea>
+                        <div class="button-container pt-1">
+                            <button id="salvar" type="submit">Enviar Mensagem</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-
-            <form action="#" class="input-container" method="post">
-
-                <div class="caixa-container row">
-                    
-                    <div class="container-titulos">
-                        <div class="titulo">
-                            <label for="nome">Nome</label>
-                            <input class="caixa" placeholder="John Doe" id="nome" rows="2"></input>
-                        </div>
-                        <div class="titulo">
-                            <label for="email">E-mail</label>
-                            <input class="caixa" id="email" placeholder="johndoe@gmail.com" rows="2"></input>
-                        </div>
-                        <div class="titulo">
-                            <label for="telefone">Telefone/WhatsApp</label>
-                            <input class="caixa" id="telefone" placeholder="(67) 99999-9999" rows="2"></input>
-                        </div>
-                    </div>
-            
-                    <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea>
-                    <div class="button-container">
-                        <button id="salvar" type="submit">Enviar Mensagem</button>
-                    </div>
-                </div>
-
-            </form>
         </div>
                 
             
