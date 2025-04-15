5a001e9348ad39cabe2cca9f65436ede915f70c8
Gabriel Paganotti <gabriel54099536@aluno.ms.senac.br>
2025-04-15 11:05:12 -0400

    Correções

--- src/pages/admin/validacaoNovoVendedor.php
+++ src/pages/admin/validacaoNovoVendedor.php
@@ -21,7 +21,11 @@ $base_url = get_base_url();
 
   <main>
 
-    <div class='roadMap'><span class='roadMap1'> Home / Painel do Administrador /</span><span class='roadMap2'>Validação de novos colaboradores</span></div>
+    <roadMap class='roadMap'>
+      <a href="<?= $base_url; ?>/src/index.php" class='roadMap1'>Home /</a>
+      <a href="#" class='roadMap1'>Painel do Administrador /</a>
+      <a class='roadMap2 redutor'>Validação de novos colaboradores</a>
+    </roadMap>
 
     <div class='corpo'>
 
@@ -29,15 +33,15 @@ $base_url = get_base_url();
         <h3 class='redutor'>Cadastro</h3>
         <a class='roadMap1'>Cadastrar novo Administrador</a>
         <a class='roadMap1'>Gerenciar meu perfil</a>
-        <h4 class='redutor'>Colaboradores</h4>
+        <h3 class='redutor'>Colaboradores</h3>
         <a class='roadMap1'>Validar novo colaborador</a>
         <a class='roadMap1'>Colaboradores aprovados</a>
         <a class='roadMap1'>Colaboradores Reprovados</a>
         <a class='roadMap1'>Listar colaboradores ativos</a>
         <a class='roadMap1'>Suporte ao colaborador</a>
-        <h4 class='redutor'>Clientes</h4>
+        <h3 class='redutor'>Clientes</h3>
         <a class='roadMap1'>Suporte ao cliente</a>
-        <h4 class='redutor'>Sistema</h4>
+        <h3 class='redutor'>Sistema</h3>
         <a class='roadMap1'>Abrir chamado</a>
         <a class='roadMap1'>Criar categoria</a>
         <a class='roadMap1'>Editar sistema</a>
@@ -52,11 +56,12 @@ $base_url = get_base_url();
 
             <div class='vendedor'>
 
+              <button class='abrirAcordeon' type="button" onclick="abrirAcordeon(0)"></button>
               <div class='acordeonFechado'>
                 <div class='bloco1'><img src="<?= $base_url; ?>/assets/img/logoEmpresaRica.jpg" class='logoEmpresa'>
                   <div class='blocoTitulo'>
-                    <span style="font-size: 32px;" class='tituloLoja'>Nome da loja</span>
-                    <div><img src='<?= $base_url; ?>/assets/img/Group.png' style="max-width: 12px; Margin-left:1%;"> Loja 1</div>
+                    <p class='tituloLoja'>Nome da loja</p>
+                    <div><img src='<?= $base_url; ?>/assets/img/Group.png' class='iconLoja'> Loja 1</div>
                   </div>
                 </div>
                 <div class='blocoAcordeon'>
@@ -67,27 +72,40 @@ $base_url = get_base_url();
 
                 <div class='acordeonAberto1'>
                   <div class='bloco1'><img src="<?= $base_url; ?>/assets/img/logoEmpresaRica.jpg" class='logoEmpresa'>
-                    <div class='blocoTitulo'><button style="border: 0px;background-color:rgb(0,0,0,0);" type='button' onclick='fecharAcordeon(0)'><span style="font-size: 32px;" class='tituloLoja'>Nome da loja</span></button></div>
+                    <div class='blocoTitulo'><button style="border: 0px;background-color:rgb(0,0,0,0);" type='button' onclick='fecharAcordeon(0)'><span class='tituloLoja'>Nome da loja</span></button></div>
                   </div>
                 </div>
 
                 <div class='acordeonAberto2'>
                   <div class='miniPainel mini1'>
-                    <div class = 'internalMini1' style="margin:25px;">
+                    <div class='internalMini1'>
 
-                      <line style="display: flex; justify-content:start;"><span class='redutor' style = 'margin-right:4px;'>Responsável: </span><span class='redutor'> Carlos</span></line>
-                      <line style="display: flex; justify-content:start;"><span class='redutor' style = 'margin-right:4px;'>Telefone: </span><span class='redutor'> (67) 3437-7000</span></line>
-                      <line style="display: flex; justify-content:start;"><span class='redutor' style = 'margin-right:4px;'>CNPJ: </span><span class='redutor'> 97.538.333/0001-00</span></line>
-                      <line style="display: flex; justify-content:start;"><span class='redutor' style = 'margin-right:4px;'>CEP: </span><span class='redutor'> 79000-00</span></line>
-                      <span class='redutor' style = 'margin-right:4px;'>E-Mail: </span><span class='redutor'> studiocenterpy@gmail.com</span>
+                      <line class = 'linha'>
+                        <p class='redutor margem4'>Responsável: </p>
+                        <p class='redutor'> Carlos</p>
+                      </line>
+                      <line class = 'linha'>
+                        <p class='redutor margem4'>Telefone: </p>
+                        <p class='redutor'> (67) 3437-7000</p>
+                      </line>
+                      <line class = 'linha'>
+                        <p class='redutor margem4'>CNPJ: </p>
+                        <p class='redutor'> 97.538.333/0001-00</p>
+                      </line>
+                      <line class = 'linha'>
+                        <p class='redutor margem4'>CEP: </p>
+                        <p class='redutor'> 79000-00</p>
+                      </line>
+                      <p class='redutor margem4'>E-Mail: </p>
+                      <p class='redutor'> studiocenterpy@gmail.com</p>
 
                     </div>
-                    <div style="margin:25px;">
-                      <line style="display: flex; justify-content:start;"><img src='<?= $base_url; ?>/assets/img/camisaIcon.svg' class='icon'><span class='iconText'> Roupas</span></line>
-                      <line style="display: flex; justify-content:start;"><img src='<?= $base_url; ?>/assets/img/cameraIcon.svg' class='icon'><span class='iconText'> Câmeras</span></line>
-                      <line style="display: flex; justify-content:start;"><img src='<?= $base_url; ?>/assets/img/celularIcon.svg' class='icon'><span class='iconText'> Smartphones</span></line>
-                      <line style="display: flex; justify-content:start;"><img src='<?= $base_url; ?>/assets/img/computadorIcon.svg' class='icon'><span class='iconText'> Computadores</span></line>
-                      <line style="display: flex; justify-content:start;"><img src='<?= $base_url; ?>/assets/img/ferramentasIcon.svg' class='icon'><span class='iconText'> Ferramentas</span></line>
+                    <div>
+                      <line class='linhaCinza'><img src='<?= $base_url; ?>/assets/img/camisaIcon.svg' class='icon'><span class='iconText'> Roupas</span></line>
+                      <line class='linhaCinza'><img src='<?= $base_url; ?>/assets/img/cameraIcon.svg' class='icon'><span class='iconText'> Câmeras</span></line>
+                      <line class='linhaCinza'><img src='<?= $base_url; ?>/assets/img/celularIcon.svg' class='icon'><span class='iconText'> Smartphones</span></line>
+                      <line class='linhaCinza'><img src='<?= $base_url; ?>/assets/img/computadorIcon.svg' class='icon'><span class='iconText'> Computadores</span></line>
+                      <line class='linhaCinza linhaCinzaBase'><img src='<?= $base_url; ?>/assets/img/ferramentasIcon.svg' class='icon'><span class='iconText'> Ferramentas</span></line>
                     </div>
                   </div>
                   <div class='miniPainel mini2'>
@@ -124,12 +142,12 @@ $base_url = get_base_url();
                   <div class='blocoFinal'>
                     <h4 class='redutor'>Observações:</h4>
                     <div><textarea name="Observações" id="observacoes" cols="30" rows="6" class='painelObservacoes' placeholder="Observações:"></textarea></div>
-                    <div class='linha'>
-                      <button class="btPadrao" style="background-color:var(--cor-botao-primario);">
-                        Reprovar
-                      </button>
-                      <button class="btPadrao" style="background-color:var(--cor-botao-adm-vermelho);">
+                    <div class='linhaBT'>
+                      <button class="btPadrao verde">
                         Aprovar
+                      </button>
+                      <button class="btPadrao vermelho">
+                        Reprovar
                       </button>
                     </div>
                   </div>
@@ -152,35 +170,4 @@ $base_url = get_base_url();
 
 </html>
 
-<script>
-  function abrirAcordeon(buttonID) {
-    console.log(buttonID);
-    const elementos = document.querySelectorAll('.vendedor');
-    console.log(elementos);
-    const elemento = elementos[buttonID];
-    console.log(elemento)
-    const elementoSelecionado1 = elemento.querySelector('.acordeonFechado');
-    const elementoSelecionado2 = elemento.querySelector('.acordeonAberto');
-    console.log(elementoSelecionado1)
-    console.log(elementoSelecionado2)
-
-    elementoSelecionado1.classList.add('removedor')
-    elementoSelecionado2.classList.add('adicionador')
-  }
-
-  function fecharAcordeon(buttonID) {
-    console.log(buttonID);
-    const elementos = document.querySelectorAll('.vendedor');
-    console.log(elementos);
-    const elemento = elementos[buttonID];
-    console.log(elemento)
-    const elementoSelecionado1 = elemento.querySelector('.acordeonFechado');
-    const elementoSelecionado2 = elemento.querySelector('.acordeonAberto');
-    console.log(elementoSelecionado1)
-    console.log(elementoSelecionado2)
-
-    elementoSelecionado1.classList.remove('removedor')
-    elementoSelecionado2.classList.add('removedor')
-    elementoSelecionado2.classList.remove('adicionador')
-  }
-</script>
+<script src='<?= $base_url; ?>/assets/js/adm/validacaoNovoVendedor/funcoesAcordeon.js'></script>
