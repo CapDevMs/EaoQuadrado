efad7a90dd9cdb8800c47cebccc53d320bd09746
Gabriel Paganotti <gabriel54099536@aluno.ms.senac.br>
2025-04-22 11:07:47 -0400

    commit

---
+++ src/pages/cliente/historicoPedidos.php
@@ -0,0 +1,69 @@
+<?php
+include('../../config/funcoes.php');
+$appName = get_app_name();
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php get_css(['base', 'style', '/cliente/historicoPedidos']) ?>
+    <title>E ao Quadrado - Histórico de pedidos</title>
+</head>
+
+<body>
+    <?php get_header() ?>
+    <main>
+        <roadMap class='roadMap'><a class='roadMap1'>Home /</a><a class='roadMap1'>Meu Perfil /</a><a>Historico de pedidos</a></roadMap>
+        <corpoPrincipal class='corpoPrincipal'>
+            <menuLateral class='redutor300'>
+                <div class='menuLateral'>
+                    <h3 class='redutor'>Cadastro</h3>
+                    <a class='roadMap1'>Cadastrar novo Administrador</a>
+                    <a class='roadMap1'>Gerenciar meu perfil</a>
+                    <h3 class='redutor'>Colaboradores</h3>
+                    <a class='roadMap1'>Validar novo colaborador</a>
+                    <a class='roadMap1'>Colaboradores aprovados</a>
+                    <a class='roadMap1'>Colaboradores Reprovados</a>
+                    <a class='roadMap1'>Listar colaboradores ativos</a>
+                    <a class='roadMap1'>Suporte ao colaborador</a>
+                    <h3 class='redutor'>Clientes</h3>
+                    <a class='roadMap1'>Suporte ao cliente</a>
+                    <h3 class='redutor'>Sistema</h3>
+                    <a class='roadMap1'>Abrir chamado</a>
+                    <a class='roadMap1'>Criar categoria</a>
+                    <a class='roadMap1'>Editar sistema</a>
+                </div>
+            </menuLateral>
+            <conteudo class="lista">
+                <div>
+                    <h2 class='redutor preto'>Histórico</h2>
+                </div>
+                <div>
+                    <div class='preto'>
+                        <div><img src='../../assets/img/acer_nitro.png'></div>
+                        <div>
+                            <h4>Nome do produto</h4>
+                            <div>AcerNitro</div>
+                        </div>
+                        <div>
+                            <h4>Data da Compra</h4>
+                            <div>22/09/96</div>
+                        </div>
+                        <div>
+                            <h4>
+                                Preço
+                            </h4>
+                        </div>
+                        <div>R$ 4680,00</div>
+                    </div>
+                </div>
+            </conteudo>
+        </corpoPrincipal>
+    </main>
+    <?php get_footer() ?>
+
+</body>
+
+</html>
