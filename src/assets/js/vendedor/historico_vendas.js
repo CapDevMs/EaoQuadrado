02250494a7d1ab7f366d28b1f8e0d56af527fb7f
annalorena22 <annalorena1998@gmail.com>
2025-04-22 16:47:00 -0400

    componente pronto para receber CSS

--- src/assets/js/vendedor/historico_vendas.js
+++ src/assets/js/vendedor/historico_vendas.js
@@ -1,32 +1,45 @@
-/**
- * 
- * @param {HTMLElement} element 
- */
-function toggleDetails(element) {
-    console.log(element)
-    element.children[6].classList.toggle("expanded");
-}
+import cardHistoricoVendas from "../components/card-historico-vendas.js";
 
-import cardHistoricoVendas from "../components/card-historico-vendas.js"
+let containerHistoricoVendas = document.querySelector('containerPedidos');
+
+let vendasHistorico = [
+  {
+    'imagem': '../../assets/img/monitor_oled.png',
+    'produto': "LCD Monitor",
+    'statusPedido': "Entregue",
+    'preco': "R$ 999,99",
+    'cliente': "Yuri Ferreira",
+    'data': "22/01/2025",
+    'quantidade': "1",
+    'endereco': "Rua Piratininga ,629, Jardim dos Estados Campo Grande",
+    'cep': "79020-240",
+  },
+  {
+    'imagem': '../../assets/img/teclado.png',
+    'produto': "Teclado Gamer",
+    'statusPedido': "Cancelado",
+    'preco': "R$ 599,99",
+    'cliente': "Paulo Morais",
+    'data': "13/09/2024",
+    'quantidade': "1",
+    'endereco': "Rua Piratininga ,629, Jardim dos Estados Campo Grande",
+    'cep': "79020-240",
+  }
+];
+
+containerHistoricoVendas.innerHTML = vendasHistorico.map(pedido => 
+  cardHistoricoVendas(pedido)
+).join('');
+
+// vendasHistorico.forEach((pedido) => {
+//   historicoVendas.innerHTML += cardHistoricoVendas(pedido)
+// })
 
 
-let produtoHistorico = document.getElementsByTagName("cardHistoricoVendas");
+window.toggleDetails = function (element) {
+  element.querySelector('.card-details').classList.toggle("expanded");
+};
 
-let produtosHistorico = [
-    {
-        'imagem' : '../img/monitor_oled.png',
-        'produto': "LCD Monitor",
-        'statusPedido' : "Entregue",
-        'preco' : "R$ 999,99",
-        'cliente' : "Yuri Ferreira",
-        'data' : "22/01/2025",
-        'quantidade' : "1",
-        'endereco' : "Rua Piratininga ,629, Jardim dos Estados Campo Grande",
-        'cep' : "79020-240"
-    }
-    
-]
-
- produtosHistorico.forEach((pedido) => {
-    produtoHistorico.innerHTML += cardHistoricoVendas(pedido)
-})
+window.salvarStatus = function(event) {
+  alert("Status alterado com sucesso!");
+};
--- src/pages/vendedor/historico_vendas.php
+++ src/pages/vendedor/historico_vendas.php
@@ -22,7 +22,6 @@ $appName = get_app_name();
 
     <div class="container col-sm-12 col-md-9">
       <h1>Histórico de Vendas</h1>
-      <!-- Cabecalho da Tabela -->
       <div class="cabecalho">
         <div class="cabecalho-item">Produto</div>
         <div class="cabecalho-item">Status do pedido</div>
@@ -32,47 +31,9 @@ $appName = get_app_name();
         <div class="cabecalho-item">Quantidade</div>
       </div>
 
-      <!-- CARDS DA TABELA -->
-      <div class="card" onclick="toggleDetails(this)">
-        <div class="cardItems cardItems-inicio">
-          <img class="img-historico" src="<?= get_base_url(); ?>/assets/img/monitor_oled.png" alt="Monitor">
-          <p class="p-50">LCD Monitor</p>
-        </div>
-        <div class="cardItems">
-          <p class="p-40">Entregue</p>
-        </div>
-        <div class="cardItems">
-          <p class="p-30">R$ 999,99</p>
-        </div>
-        <div class="cardItems">
-          <p class="p-20">Yuri Ferreira</p>
-        </div>
-        <div class="cardItems">
-          <p class="p-data">22/01/2025</p>
-        </div>
-        <div class="cardItems">
-          <p class="p-10">1</p>
-        </div>
-        <div class="card-details">
-          <div class="elementCard">
-            <p class="clienteP">Cliente: <span class="nomeP">Yuri Ferreira</span> </p>
-            <p class="clienteP">Endereço: <span class="nomeP"> Rua Piratininga ,629, Jardim dos Estados Campo Grande
-                MS</span> </p>
-            <p class="clienteP">Data: <span class="nomeP">12 de janeiro</span></p>
-            <p class="clienteP">CEP: <span class="nomeP">79020-240</span></p>
-          </div>
-
-          <label for="statusPedido">Status de Pedido</label>
-          <select id="statusPedido" name="statusPedido">
-            <option value="entregue">Entregue</option>
-            <option value="opcao2">Troca</option>
-            <option value="opcao3">Cancelamento</option>
-          </select>
-          <span class="clienteP">12 de dezembro 2024</span>
-        </div>
+      <div class="container-historico">
+        <containerPedidos></containerPedidos>
       </div>
-
-      <cardHistoricoVendas></cardHistoricoVendas>
 
     </div>
   </main>
