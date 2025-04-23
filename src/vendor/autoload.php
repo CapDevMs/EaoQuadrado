b5e005c4b99935b476b742bb9578ae2e62d29baf
Felipe Braiani <felipe.braianitech@gmail.com>
2025-04-23 11:12:23 -0400

    Começando o autoload das classes PHP

---
+++ src/vendor/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+spl_autoload_register(function($className) {
+
+    $paths = [
+        join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'App', 'Controllers')),
+        join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'App', 'Models')),
+        join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'App', 'helpers')),
+    ];
+
+    foreach ($paths as $path) {
+        $file = $path . $className . '.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    }
+});
