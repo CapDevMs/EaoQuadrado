c3046641017debfe5f468c2d2da9fd86144afd29
--global <--global>
2025-04-30 11:01:08 -0400

    forms arrumado PS: Falta o menu lateral

--- src/pages/admin/abertura-de-chamados.php
+++ src/pages/admin/abertura-de-chamados.php
@@ -14,39 +14,46 @@
     <?php get_header() ?>
 
     <main class="row">    
-        <div class="col-md-1 col-sm-hidden">
+        <div class="col-md-2 col-sm-hidden">
             <a id="menu" href="../../index.php">Pagina Inicial</a>
             <span>/</span>
             <a id="sub-menu" href="index_adm.php">Painel do administrador</a>
             <span>/</span>
             <a id="sub-menu1" href="abertura-de-chamados.php">Abrir chamado</a>
             
-            <?php get_sidebar_adm('abrir_chamado'); ?>
+            <?php get_sidebar_adm('abrir_chamado'); ?> 
         </div>
 
         
-        <div class="col-md-9 col-sm-12">
+        <div class="container-form col-md-8 col-sm-12">
             <form action="#" method="get">
                 <div class="titulo">
                     <label>Nome Completo</label><br>
-                    <textarea class="caixa" placeholder="John" id="nome" rows="2"></textarea>
+                    <input type="text" class="caixa" placeholder="John" id="nome">
                 </div>
                 <div class="titulo">
                     <label>Contato</label><br>
-                    <textarea class="caixa" id="email" placeholder="(67) 99999-9999" rows="2"></textarea>
+                    <input type="text" class="caixa" id="email" placeholder="(67) 99999-9999">
                 </div>
                 <div class="titulo">
                     <label>Título</label><br>
-                    <textarea class="caixa" id="telefone" placeholder="Erro ao cadastrar novo administrador" rows="2"></textarea>
+                    <input type="text" class="caixa" id="telefone" placeholder="Erro ao cadastrar novo administrador">
+                </div>
+
+                <div id="div-descricao">
+                    <label>Descrição do Chamado</label><br>
+                    <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea><br><br>
                 </div>
             </form>
-            <div class="descricao">
-                    <label>Descrição do Chamado</label>
-                    <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea><br><br>
-            </div>
             
-            <input type="file" id="arquivo" name="arquivo" accept="image/*" multiple><br><br>
+        
+            <div class="anexar-arquivo">
+                <label for="arquivo">Anexar Arquivo</label>
+                <input type="file" id="arquivo" name="arquivo" accept="image/*" multiple>
+            </div>
+
             <button id="salvar" type="submit">Enviar Mensagem</button>
+
             <div id="modalImagem" class="modal">
                 <div class="modal-content">
                     <span class="fechar">&times;</span>
