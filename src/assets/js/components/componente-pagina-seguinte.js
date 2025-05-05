1149b2a72e0beb2a8d667d6b46bc9af1e667bbb2
Ricardo <sousasilvaricardo9@gmail.com>
2025-05-05 00:56:33 -0400

    terminando o front-end do componente trocar página

--- src/assets/js/components/componente-pagina-seguinte.js
+++ src/assets/js/components/componente-pagina-seguinte.js
@@ -1,18 +1,16 @@
-// componente da pagina seguinte aqui   
-
-const paginaSeguinte = ({link}) => {
+// componente-pagina-seguinte.js
+const paginaSeguinte = ({ link }) => {
     return `
-        <nav class="container-componente-page-seguinte row"> 
-            <ul class="number-pages">
-                <li><a href="${link}" id="number-page-1">1</a></li>
-                <li><a href="${link}" id="number-page-2">2</a></li>
-                <li><a href="${link}" id="number-page-3">3</a></li>
-                <li><a href="${link}" id="number-page-4">4</a></li>
-                <li><a href="${link}" id="page-seguinte">Seguinte ></a></li>
-            </ul>
-        </nav>
+      <nav class="container-componente-page-seguinte row"> 
+        <ul class="number-pages">
+          <li><a href="${link}" id="page-anterior">&#60; Anterior</a></li>
+          <li><a href="${link}" data-page="1" id="number-page-1">1</a></li>
+          <li><a href="${link}" data-page="2" id="number-page-2">2</a></li>
+          <li><a href="${link}" data-page="3" id="number-page-3">3</a></li>
+          <li><a href="${link}" data-page="4" id="number-page-4">4</a></li>
+          <li><a href="${link}" id="page-seguinte">Seguinte &#62;</a></li>
+        </ul>
+      </nav>
     `;
   };
-  
   export default paginaSeguinte;
- 
