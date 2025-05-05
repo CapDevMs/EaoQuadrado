40c6ecd51e24630ccb8691714a3eaa19bd91ac8e
Vatreax <rafasmontiel13@gmail.com>
2025-05-05 11:05:00 -0400

    JavaScript

--- src/assets/js/adm/button_style.js
+++ src/assets/js/adm/button_style.js
@@ -1,14 +1,32 @@
+
+function resetButtons() {
+    document.querySelectorAll('.filter-button').forEach(function (button) {
+        button.classList.remove('ativo');
+    });
+}
+
+
 document.getElementById("todos").addEventListener("click", function () {
-    this.classList.toggle("ativo");
-    document.querySelector(".perfil").classList.remove("hidden");
+    resetButtons(); 
+    this.classList.add("ativo");
+    document.querySelector(".table").classList.remove("hidden"); 
+    document.querySelector(".approved").classList.remove("hidden")
+    document.querySelector(".repproved").classList.remove("hidden");
 });
 
+
 document.getElementById("aprovado").addEventListener("click", function () {
-    this.classList.toggle("ativo");
-    document.querySelector(".perfil").classList.remove("hidden"); 
+    resetButtons(); 
+    this.classList.add("ativo");
+    document.querySelector(".approved").classList.remove("hidden")
+    document.querySelector(".repproved").classList.add("hidden"); 
+    document.querySelector(".table").classList.remove("hidden"); 
 });
 
 document.getElementById("reprovado").addEventListener("click", function () {
-    this.classList.toggle("ativo");
-    document.querySelector(".perfil").classList.add("hidden");
+    resetButtons(); 
+    this.classList.add("ativo");
+    document.querySelector(".approved").classList.add("hidden");
+    document.querySelector(".repproved").classList.remove("hidden");
+    document.querySelector(".table").classList.remove("hidden"); 
 });
--- src/pages/admin/contas_aprovadas_reprovadas.php
+++ src/pages/admin/contas_aprovadas_reprovadas.php
@@ -19,36 +19,39 @@
                 <div class="selector col-sm-5">
                     <h1>Histórico de Contas</h1>
                     <div class="status row">
-                        <button class="btn-status col-sm-3 ativo" id="todos">Todos</button>
-                        <button class="btn-status col-sm-3" id="aprovado">Aprovadas</button>
-                        <button class="btn-status col-sm-3" id="reprovado">Reprovadas</button>
+                        <button class="btn-status filter-button col-sm-3 ativo" id="todos">Todos</button>
+                        <button class="btn-status filter-button col-sm-3" id="aprovado">Aprovadas</button>
+                        <button class="btn-status filter-button col-sm-3" id="reprovado">Reprovadas</button>
                     </div>
                 </div>
 
             
                 <div class="container col-sm-9">
                     <div class="perfis-body container col-sm-12 row">
-                        <div class="interior container col-sm-10"> 
-                            <div class="perfil col-sm-11 mlr-1" id="approved">
-                                <div class="foto-container col-sm-12 col-xl-2">
-                                    <img src="<?= get_base_url() ?>assets\img\foto-perfil.png" alt="foto-perfil">
-                                </div>
-                                <div class="col-sm-12 col-xl-5 ">
-                                    <h1>Carlos Santana da Silva</h1>
-                                    <p>carlos_silva@gmail.com</p>
-                                </div>
-                                <div class="aprovado col-sm-12 col-xl-5">
-                                    <img src="<?= get_base_url() ?>assets\img\icons\icon-cancel.svg" alt="">
-                                    <p>Reprovado em 27/09/2020</p>
+
+                        <div class="table col-sm-12 row">
+                            <div class="interior container col-sm-10"> 
+                                <div class="perfil col-sm-11 mlr-1" id="approved">
+                                    <div class="foto-container col-sm-12 col-xl-2">
+                                        <img src="<?= get_base_url() ?>assets\img\foto-perfil.png" alt="foto-perfil">
+                                    </div>
+                                    <div class="col-sm-12 col-xl-5 ">
+                                        <h1>Carlos Santana da Silva</h1>
+                                        <p>carlos_silva@gmail.com</p>
+                                    </div>
+                                    <div class="aprovado col-sm-12 col-xl-5">
+                                        <img src="<?= get_base_url() ?>assets\img\icons\icon-cancel.svg" alt="">
+                                        <p>Reprovado em 27/09/2020</p>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div class="visualizar col-sm-2">
+                                <a>Visualizar Perfil</a>
+                            </div>
+
+
                         </div>
-
-                        <div class="visualizar col-sm-2">
-                            <a>Visualizar Perfil</a>
-                        </div>
-
-
                     </div>
                 </div>
             </div>
