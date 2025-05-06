8476b0fd5b14c1968d32fcd9189d1eb0687e3c7f
Ricardo <sousasilvaricardo9@gmail.com>
2025-05-06 11:08:59 -0400

    terminando de passar para o modelo mvc e ajustando detalhes de reponsividade da tela favoritos

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,11 +3,13 @@
 use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use App\Controllers\ProdutoController;
+use App\Controllers\favoritosController;
 use Core\Router;
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
+$router->get('/favoritos', favoritosController::class, 'favoritos');
 $router->get('/produto', ProdutoController::class, 'produto');
 $router->get('/logout', AuthController::class, 'logout');
 $router->get('/login', AuthController::class, 'index');
