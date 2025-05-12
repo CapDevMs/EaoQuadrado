509836dec696efdb2c864529e295f701447479ee
Vatreax <rafasmontiel13@gmail.com>
2025-05-12 08:13:24 -0400

    Incompleto

--- src/public/assets/js/adm/filtro_aprovados.js
+++ src/public/assets/js/adm/filtro_aprovados.js
@@ -1,12 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.btn-status');
+function toggleAccordion(button) {
+    var parentAccordion = button.closest('.accordion');
+    var content = parentAccordion.querySelector('.accordion-content');
+    var paragraph = parentAccordion.querySelector('p');
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-
-            buttons.forEach(btn => btn.classList.remove('ativo'));
-
-            button.classList.add('ativo');
-        });
-    });
-});
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        parentAccordion.classList.remove("open");
+        paragraph.style.display = "block";
+    } else {
+        content.style.display = "block";
+        parentAccordion.classList.add("open");
+        paragraph.style.display = "none";
+    }
+}
