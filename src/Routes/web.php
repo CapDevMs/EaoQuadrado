9729fa6e1150f4727d4de85736486136f0d5174a
Ricardo <sousasilvaricardo9@gmail.com>
2025-05-12 09:19:35 -0400

    apagando arquivo a mais e reestruturando para mvc novamente

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -4,12 +4,14 @@ use App\Controllers\AuthController;
 use App\Controllers\HomeController;
 use App\Controllers\ProdutoController;
 use App\Controllers\meuPerfilAdmController;
+use App\Controllers\favoritosController;
 use Core\Router;
 
 $router = new Router();
 
 $router->get('/', HomeController::class, 'index');
 $router->get('/perfilAdm', meuPerfilAdmController::class, 'perfilAdm');
+$router->get('/favoritos', favoritosController::class, 'favoritos');
 $router->get('/produto', ProdutoController::class, 'produto');
 $router->get('/logout', AuthController::class, 'logout');
 $router->get('/login', AuthController::class, 'index');
