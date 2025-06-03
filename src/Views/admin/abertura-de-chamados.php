95694ae6974a3c154d0df08c7a304377aa6a548e
joao772003 <joao54099736@aluno.ms.senac.br>
2025-06-03 00:07:57 -0400

    refactor(admin): Tela Abertura de Chamados Finalizada

--- src/Views/admin/abertura-de-chamados.php
+++ src/Views/admin/abertura-de-chamados.php
@@ -15,7 +15,7 @@
     <main>    
         <div class="row">
             <h1 id="abertura-title">Abertura de Chamados</h1>
-            <div class="col-md-3 col-sm-hidden">
+            <div class="area-side col-md-3 col-sm-hidden">
 
                 <?php get_sidebar_admin('abrir_chamado'); ?> 
             </div>
