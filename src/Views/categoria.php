86191120a6c643a62fa75d3a508ccdf6431dbdd2
Vatreax <rafasmontiel13@gmail.com>
2025-06-05 08:15:47 -0400

    WIP

--- src/Views/categoria.php
+++ src/Views/categoria.php
@@ -10,8 +10,7 @@ $appName = get_app_name();
     <title>E ao Quadrado - O melhor E-commerce</title>
     <!-- <title><?= $appName . ' - ' . $titulo ?? '' ?></title> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
-    <?php get_css(['filtro_categoria', 'cliente/categoria']) ?>
-    <?php get_css(['style', 'base','cliente/categoria']) ?>
+    <?php get_css(['filtro_categoria', 'cliente/categoria', 'base', 'style']) ?>
     <?php get_css_components() ?>
 </head>
 
