89da10109454d6d8ab4424203a152bceac9320c6
joao772003 <joao54099736@aluno.ms.senac.br>
2025-06-08 22:55:47 -0400

    refactor: Tela arrumada

--- src/Views/admin/abertura-de-chamados.php
+++ src/Views/admin/abertura-de-chamados.php
@@ -1,5 +1,7 @@
 <?php
     $appName = get_app_name();
+    $baseUrl = get_base_url();
+    $titulo = "Abertura de Chamados";
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -15,43 +17,42 @@
     <main>    
         <div class="row">
             <h1 id="abertura-title">Abertura de Chamados</h1>
-            <div class="area-side col-md-3 col-sm-hidden">
 
+            <div class="area-side col-md-3 col-sm-hidden">
                 <?php get_sidebar_admin('abrir_chamado'); ?> 
             </div>
 
             
-            <div class="container-form col-md-8 col-sm-12">
+            <div class="container-form col-md-8 col-sm-12 ml-2">
                 <form action="#" method="get">
                     <div class="titulo">
-                        <label>Nome Completo</label><br>
+                        <label>Nome Completo</label>
                         <input type="text" class="caixa" placeholder="John" id="nome">
                     </div>
                     <div class="titulo">
-                        <label>Contato</label><br>
+                        <label>Contato</label>
                         <input type="text" class="caixa" id="email" placeholder="(67) 99999-9999">
                     </div>
                     <div class="titulo">
-                        <label>Título</label><br>
+                        <label>Título</label>
                         <input type="text" class="caixa" id="telefone" placeholder="Erro ao cadastrar novo administrador">
                     </div>
 
                     <div id="div-descricao">
-                        <label>Descrição do Chamado</label><br>
-                        <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea><br><br>
+                        <label>Descrição do Chamado</label>
+                        <textarea class="caixa2" id="descricao" placeholder="Sua mensagem" rows="15" required></textarea>
+                    </div>
+                    
+                    <div class="anexar-arquivo">
+                        <label for="arquivo">Anexar Arquivo</label>
+                        <input type="file" id="arquivo" name="arquivo" accept="image/*" multiple>
                     </div>
                 </form>
                 
-            
-                <div class="anexar-arquivo">
-                    <label for="arquivo">Anexar Arquivo</label>
-                    <input type="file" id="arquivo" name="arquivo" accept="image/*" multiple>
-                </div>
-
                 <div id="div-salvar">
                     <button id="salvar" type="submit">Enviar Mensagem</button>
                 </div>
-
+            
                 <div id="modalImagem" class="modal">
                     <div class="modal-content">
                         <span class="fechar">&times;</span>
