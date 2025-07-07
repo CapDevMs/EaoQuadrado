d1370744814e849e78509625a5b804008ea7ceb0
Ricardo <sousasilvaricardo9@gmail.com>
2025-07-07 11:12:54 -0400

    feat(WIP): ajustando card da pagina do vendedor

--- src/Views/vendedor/pagina_do_vendedor.php
+++ src/Views/vendedor/pagina_do_vendedor.php
@@ -11,6 +11,7 @@ $titulo = 'Página do Vendedor';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="<?= get_base_url(); ?>assets/img/logo.png">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
   <?php get_css(['cliente/pagina_vendedor','vendedor/pagina_do_vendedor']) ?>
   <?php get_css_components(); ?>
 
@@ -29,11 +30,56 @@ $titulo = 'Página do Vendedor';
           </div>
         </div>
         <div class="row">
-          <div class="col-sm-12 pb-4">
-            <banner_vendedor class="card">
-            </banner_vendedor>
+        <div class="col-sm-12 pb-4">
+          <div class="card">
+              <div class="card-body justify-content-center ptb-2">
+                  <div class="col-2 justify-content-center">
+                      <div class="avatar">
+                          <img src="../assets/img/cliente/studio_center.svg" alt="" class="figure-img img-fluid">
+                      </div>
+                  </div>
+                  <div class="col-7">
+                      <div class="row">
+                          <div class="col-12">
+                              <h2>${titulo}</h2>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-12">
+                              <a href="mailto:studiocenterpy@gmail.com">${email}</a>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-12">
+                              <p>${endereco}</p>
+                              <p>Campo Grande - MS</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-3">
+                      <div class="row">
+                          <div class="col-12 text-right stars">
+                              <p class="mr-1">Avaliações: </p>
+                              <p class="card_estrela_texto estrela_35">★★★★★</p>
+                          </div>
+                      </div>
+                      <div class="icon-favoritar">
+                          <div class="col-12 text-right">
+                              <img class="whatsapp" src="../assets/img/icons/whatsapp.svg" alt="">
+                              <a target="_blank" href="https://wa.me/556734377000">${telefone}</a>
+                          </div>
+                      </div>
+                      <!-- botao "gostei" --- favoritar  -->
+                      <div class="row">
+                          <div class="col-12 icone float-right">
+                              <img src="../assets/img/icons/coracao.svg" alt="Favoritar" class="icone_selecionado">
+                          </div>
+                      </div>
+                  </div>
+                </div>
+              </div>  
+            </div>
           </div>
-        </div>
 
         <div class="row">
           <div class="col-sm-12">
