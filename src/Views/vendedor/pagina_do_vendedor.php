dd7e9debff80ea3f6cc16fc166df89ebe5ab44ba
Ricardo <sousasilvaricardo9@gmail.com>
2025-07-08 10:55:29 -0400

    feat(WIP): ajustando card vendedor

--- src/Views/vendedor/pagina_do_vendedor.php
+++ src/Views/vendedor/pagina_do_vendedor.php
@@ -30,54 +30,40 @@ $titulo = 'Página do Vendedor';
           </div>
         </div>
         <div class="row">
-        <div class="col-sm-12 pb-4">
-          <div class="card">
-              <div class="card-body justify-content-center ptb-2">
-                  <div class="col-2 justify-content-center">
-                      <div class="avatar">
-                          <img src="../assets/img/cliente/studio_center.svg" alt="" class="figure-img img-fluid">
-                      </div>
+          <div class="col-sm-12 pb-4">
+            <div class="card position-relative"><!-- necessário para que o ícone seja posicionado em relação ao card -->
+              <div class="card-body ptb-2">
+                <div class="row align-items-center">
+                  <!-- Avatar -->
+                  <div class="col-2 text-center">
+                    <div class="avatar">
+                      <img src="../assets/img/cliente/studio_center.svg" alt="imagem loja" class="figure-img img-fluid">
+                    </div>
                   </div>
+
+          <!-- Informações da loja -->
                   <div class="col-7">
-                      <div class="row">
-                          <div class="col-12">
-                              <h2>${titulo}</h2>
-                          </div>
-                      </div>
-                      <div class="row">
-                          <div class="col-12">
-                              <a href="mailto:studiocenterpy@gmail.com">${email}</a>
-                          </div>
-                      </div>
-                      <div class="row">
-                          <div class="col-12">
-                              <p>${endereco}</p>
-                              <p>Campo Grande - MS</p>
-                          </div>
-                      </div>
+                    <h2>${titulo}</h2>
+                    <a href="mailto:studiocenterpy@gmail.com">${email}</a>
+                    <p>${endereco}</p>
+                    <p>Campo Grande - MS</p>
                   </div>
-                  <div class="col-3">
-                      <div class="row">
-                          <div class="col-12 text-right stars">
-                              <p class="mr-1">Avaliações: </p>
-                              <p class="card_estrela_texto estrela_35">★★★★★</p>
-                          </div>
-                      </div>
-                      <div class="icon-favoritar">
-                          <div class="col-12 text-right">
-                              <img class="whatsapp" src="../assets/img/icons/whatsapp.svg" alt="">
-                              <a target="_blank" href="https://wa.me/556734377000">${telefone}</a>
-                          </div>
-                      </div>
-                      <!-- botao "gostei" --- favoritar  -->
-                      <div class="row">
-                          <div class="col-12 icone float-right">
-                              <img src="../assets/img/icons/coracao.svg" alt="Favoritar" class="icone_selecionado">
-                          </div>
-                      </div>
+
+                  <!-- Avaliações e WhatsApp -->
+                  <div class="col-3 text-right">
+                    <p class="mr-1">Avaliações: </p>
+                    <p class="card_estrela_texto estrela_35">★★★★★</p>
+                    <img class="whatsapp" src="../assets/img/icons/whatsapp.svg" alt="">
+                    <a target="_blank" href="https://wa.me/556734377000">${telefone}</a>
                   </div>
                 </div>
-              </div>  
+              </div>
+
+              <!-- Ícone de coração no canto inferior direito -->
+                <div class="position-icone-favoritar mb-2 pr-2">
+                  <img src="../assets/img/icons/coracao.svg" alt="Favoritar" class="icone-favoritar">
+                </div>
+              </div>
             </div>
           </div>
 
@@ -89,7 +75,7 @@ $titulo = 'Página do Vendedor';
             </div>
           </div>
         </div>
-        <card-produto class="row"></card-produto>
+        <card-produto class="row"></card-produto> 
       </div>
       <div class="container-comentarios">
         <container-comentarios>
