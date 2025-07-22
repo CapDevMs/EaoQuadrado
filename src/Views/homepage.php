5b23afc7b0cf791de5c6ba3265f30c9edc09fe9a
Vatreax <rafasmontiel13@gmail.com>
2025-07-22 07:11:06 -0400

    mosaico

--- src/Views/homepage.php
+++ src/Views/homepage.php
@@ -50,11 +50,11 @@ $titulo = 'Homepage';
                 </div>
 
                 <div class="mosaico row col-sm-12">
-                    <img class="col-sm-12 col-md-12 col-xl-6 itens1" src="src/public/assets/img/mosaico1.png" alt="imagem 1 do mosaico" class="itens1" id="item1" onclick="window.location.href='<?= get_base_url(); ?>produto'"></img> 
+                    <img class="col-sm-12 col-md-12 col-xl-6 itens1" src="src/public/assets/img/mosaico1.png" alt="imagem 1 do mosaico" class="itens1" id="item1" onclick="window.location.href='<?= get_base_url(); ?>categoria'"></img> 
                     <div class="col-sm-12 col-md-12 col-xl-6">
                         <img class="col-sm-12 itens2" src="src/public/assets/img/mosaico2.png" alt="imagem 2 do mosaico" class="itens2" id="item2" onclick="window.location.href='<?= get_base_url(); ?>categoria'"></img>
                         <div class="itens3-4 col-sm-12 pt-1">
-                            <img class="col-sm-6 itens3" src="src/public/assets/img/mosaico3.png" alt="imagem 3 do mosaico" class="itens3" id="item3" onclick="window.location.href='<?= get_base_url(); ?>produto'"></img>
+                            <img class="col-sm-6 itens3" src="src/public/assets/img/mosaico3.png" alt="imagem 3 do mosaico" class="itens3" id="item3" onclick="window.location.href='<?= get_base_url(); ?>categoria'"></img>
 
                             <img class="col-sm-6 itens4" src="src/public/assets/img/mosaico4.png" alt="imagem 4 do mosaico" class="itens4" id="item4" onclick="window.location.href='<?= get_base_url(); ?>produto'"></img>
                         </div>
