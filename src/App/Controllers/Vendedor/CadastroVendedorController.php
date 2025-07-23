00f2daf4c4bb9118962dff43d1015915235c607c
annalorena22 <annalorena1998@gmail.com>
2025-07-23 00:15:00 -0400

    tentando finalizar cadastro de vendedor

--- src/App/Controllers/Vendedor/CadastroVendedorController.php
+++ src/App/Controllers/Vendedor/CadastroVendedorController.php
@@ -18,6 +18,104 @@ class CadastroVendedorController extends Controller
         View::render('cadastros/cadastro_vendedor');
     }
 
+    public function salvarCadastroVendedor()
+    {
+        $dadosVendedor = [
+            "nome" => htmlspecialchars($_POST['nome']),
+            "sobrenome" => htmlspecialchars($_POST['sobrenome']),
+            "nascimento" => (int) $_POST['nascimento'],
+            "cpf" => (int) $_POST['cpf'],
+            "email" => $_POST['email'],
+            "telefone" => (int) $_POST['telefone'],
+            "cep" => (int) $_POST['cep'],
+            "endereco" => htmlspecialchars($_POST['endereco']),
+            "bairro" => htmlspecialchars($_POST['bairro']),
+            "complemento" => htmlspecialchars($_POST['complemento']),
+            "senha" => $_POST['senha'],
+
+            "nome_loja" => htmlspecialchars($_POST['nome_loja']),
+            "endereco_loja" => htmlspecialchars($_POST['endereco_loja']),
+            "cnpj" => htmlspecialchars($_POST['cnpj']),
+            "email_loja" => $_POST['email_loja'],
+            "cep_loja" => (int) $_POST['cep_loja'],
+            "bairro_loja" => htmlspecialchars($_POST['bairro_loja']),
+            "telefone_loja" => (int) $_POST['telefone_loja'],
+            "numero_endereco" => (int) $_POST['numero_endereco'],
+            "rede_social" => htmlspecialchars($_POST['rede_social'])
+        ];
+
+        $validacao = $this->validarDados($dadosVendedor);
+
+        try {
+            $user = new User();
+            $idUsuario = $user->cadastroUsuarios(
+                $dadosVendedor['nome'] . ' ' . $dadosVendedor['sobrenome'],
+                password_hash($validacao['senha'], PASSWORD_DEFAULT),
+                "vendedor"
+            );
+
+            if (!$idUsuario) {
+                throw new \Exception('Falha ao criar usuário');
+            }
+
+            $endereco = new Endereco();
+            $idEndereco = $endereco->cadastroEndereco(
+                $dadosVendedor['cep'],
+                $dadosVendedor['endereco'],
+                $dadosVendedor['bairro'],
+                $dadosVendedor['complemento']
+            );
+
+            var_dump($idEndereco);
+            if (!$idEndereco) {
+                throw new \Exception('Falha ao criar endereço.');
+            }
+
+            $vendedor = new Vendedor();
+            $idVendedor = $vendedor->cadastroVendedor(
+                $idEndereco,
+                $idUsuario,
+                $dadosVendedor['nome'],
+                $dadosVendedor['sobrenome'],
+                $dadosVendedor['cpf'],
+                $dadosVendedor['nascimento'],
+                $dadosVendedor['telefone'],
+                $dadosVendedor['cep'],
+                $dadosVendedor['endereco'],
+                $dadosVendedor['numero_endereco'],
+                $dadosVendedor['complemento'],
+                $dadosVendedor['bairro'],
+                $dadosVendedor['email'],
+                password_hash($dadosVendedor['senha'], PASSWORD_DEFAULT),
+                $dadosVendedor['nome_loja'],
+                $dadosVendedor['endereco_loja'],
+                $dadosVendedor['cnpj'],
+                $dadosVendedor['email_loja'],
+                $dadosVendedor['cep_loja'],
+                $dadosVendedor['bairro_loja'],
+                $dadosVendedor['complemento_loja'],
+                $dadosVendedor['telefone_loja'],
+                $dadosVendedor['numero_endereco'],
+                $dadosVendedor['rede_social']
+            );
+
+            var_dump($idVendedor);
+
+            if (!$idVendedor) {
+                $user->delete($idUsuario);
+                $endereco->delete($idEndereco);
+                throw new \Exception('Falha ao criar vendedor.');
+            }
+
+            $successMessage = "Cadastro de vendedor realizado com sucesso!";
+
+            return redirect()->route('cadastroVendedor')->with('success', $successMessage);
+        } catch (Exception $error) {
+            session()->flash('error', 'Erro ao salvar o cadastro: ' . $error->getMessage());
+            return redirect()->route('cadastroVendedor')->with('errors', ['Erro ao salvar o cadastro.']);
+        }
+    }
+
     private function validarDados()
     {
         $validacao = [
@@ -48,130 +146,10 @@ class CadastroVendedorController extends Controller
         $validator = new Validator($this->request->all(), $validacao);
 
         if ($validator->fails()) {
-
+            echo "Erro de validação: " . implode(', ', $validator->errors());
             session()->flash('error', 'Alguns campos não foram preenchidos corretamente.');
 
             return redirect()->route('cadastroVendedor')->with('errors', $validator->errors());
-        }
-    }
-
-    public function salvarCadastroVendedor()
-    {
-        $dadosVendedor = [
-            "nome" => htmlspecialchars($_POST['nome']),
-            "sobrenome" => htmlspecialchars($_POST['sobrenome']),
-            "nascimento" => (int) $_POST['nascimento'],
-            "cpf" => (int) $_POST['cpf'],
-            "email" => $_POST['email'],
-            "telefone" => (int) $_POST['telefone'],
-            "cep" => (int) $_POST['cep'],
-            "endereco" => htmlspecialchars($_POST['endereco']),
-            "bairro" => htmlspecialchars($_POST['bairro']),
-            "complemento" => htmlspecialchars($_POST['complemento']),
-            "senha" => $_POST['senha'],
-
-            "nome_loja" => htmlspecialchars($_POST['nome_loja']),
-            "endereco_loja" => htmlspecialchars($_POST['endereco_loja']),
-            "cnpj" => htmlspecialchars($_POST['cnpj']),
-            "email_loja" => $_POST['email_loja'],
-            "cep_loja" => (int) $_POST['cep_loja'],
-            "bairro_loja" => htmlspecialchars($_POST['bairro_loja']),
-            "telefone_loja" => (int) $_POST['telefone_loja'],
-            "numero_endereco" => (int) $_POST['numero_endereco'],
-            "rede_social" => htmlspecialchars($_POST['rede_social'])
-        ];
-        $validacao = $this->validarDados(
-            $dadosVendedor['nome'],
-                $dadosVendedor['sobrenome'],
-                $dadosVendedor['cpf'],
-                $dadosVendedor['nascimento'],
-                $dadosVendedor['telefone'],
-                $dadosVendedor['cep'],
-                $dadosVendedor['endereco'],
-                $dadosVendedor['numero_endereco'],
-                $dadosVendedor['complemento'],
-                $dadosVendedor['bairro'],
-                $dadosVendedor['email'],
-                password_hash($dadosVendedor['senha'], PASSWORD_DEFAULT),
-                $dadosVendedor['nome_loja'],
-                $dadosVendedor['endereco_loja'],
-                $dadosVendedor['cnpj'],
-                $dadosVendedor['cep_loja'],
-                $dadosVendedor['bairro_loja'],
-                $dadosVendedor['telefone_loja'],
-                $dadosVendedor['numero_endereco'],
-                $dadosVendedor['rede_social']
-        );
-
-        try {
-
-            $user = new User();
-            $idUsuario = $user->cadastroUsuarios(
-                $validacao['nome'],
-                $validacao['email'],
-                password_hash($validacao['senha'], PASSWORD_DEFAULT),
-                "vendedor"
-            );
-
-            if (!$idUsuario) {
-                throw new \Exception('Falha ao criar usuário');
-            }
-
-            $endereco = new Endereco();
-            $idEndereco = $endereco->cadastroEndereco(
-                $validacao['cep'],
-                $validacao['endereco'],
-                $validacao['bairro'],
-                $validacao['complemento']
-            );
-
-            var_dump($idEndereco);
-            if (!$idEndereco) {
-                throw new \Exception('Falha ao criar endereço.');
-            }
-
-            $vendedor = new Vendedor();
-            $idVendedor = $vendedor->cadastroVendedor(
-                $idEndereco,
-                $idUsuario,
-                $validacao['nome'],
-                $validacao['sobrenome'],
-                $validacao['cpf'],
-                $validacao['nascimento'],
-                $validacao['telefone'],
-                $validacao['cep'],
-                $validacao['endereco'],
-                $validacao['numero_endereco'],
-                $validacao['complemento'],
-                $validacao['bairro'],
-                $validacao['email'],
-                password_hash($validacao['senha'], PASSWORD_DEFAULT),
-                $validacao['nome_loja'],
-                $validacao['endereco_loja'],
-                $validacao['cnpj'],
-                $validacao['email_loja'],
-                $validacao['cep_loja'],
-                $validacao['bairro_loja'],
-                $validacao['complemento_loja'],
-                $validacao['telefone_loja'],
-                $validacao['numero_endereco'],
-                $validacao['rede_social']
-            );
-
-            var_dump($vendedor);
-
-            if (!$idVendedor) {
-                $user->delete($idUsuario);
-                $endereco->delete($idEndereco);
-                throw new \Exception('Falha ao criar vendedor.');
-            }
-
-            $successMessage = "Cadastro de vendedor realizado com sucesso!";
-
-            return redirect()->route('cadastroVendedor')->with('success', $successMessage);
-        } catch (Exception $error) {
-            session()->flash('error', 'Erro ao salvar o cadastro: ' . $error->getMessage());
-            return redirect()->route('cadastroVendedor')->with('errors', ['Erro ao salvar o cadastro.']);
         }
     }
 }
--- src/Views/cadastros/cadastro_vendedor.php
+++ src/Views/cadastros/cadastro_vendedor.php
@@ -23,7 +23,7 @@ $titulo = 'Cadastro de Vendedor';
         breadcrumb(array('http://localhost/EaoQuadrado/src/' => 'Home', 'http://localhost/EaoQuadrado/src/pages/contato.php' => 'Contato'));
         ?></div>
 
-        <form method="post" action="<?= get_base_url(); ?>cadastroVendedor" enctype="multipart/form-data">
+        <form method="post" action="<?= get_base_url(); ?>vendedor/cadastroVendedor" enctype="multipart/form-data">
             <div class="register_client" id="pessoal">
                 <h2>Cadastro de Vendedor</h2>
                 <div class="form-container">
